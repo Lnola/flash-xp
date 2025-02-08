5b3be311ad57d7064c273b9a9589bea20c22a64c
Lnola <nola.luka@gmail.com>
2025-02-08 23:53:34 +0100

    🚧 Add the flashcard widget to the main

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'widgets/flashcard-mcq/flashcard_mcq.dart';
+
 void main() {
   runApp(const MainApp());
 }
@@ -9,10 +11,26 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    var question = 'What is the spanish word for apple?';
+    var options = ['Manzana', 'Naranja', 'Plátano', 'Fresa'];
+    var correctOption = 'Manzana';
+
+    void getNextQuestion() {}
+
+    return MaterialApp(
+      title: "flash-xp",
+      theme: ThemeData(
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
+      ),
       home: Scaffold(
         body: Center(
-          child: Text('Hello World!'),
+          child: FlashcardMcq(
+            question: question,
+            options: options,
+            correctOption: correctOption,
+            onNext: getNextQuestion,
+          ),
         ),
       ),
     );
