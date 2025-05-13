087a30e30a6d0a9b06a2ef5156ff087e8c50df53
Lnola <nola.luka@gmail.com>
2025-05-13 23:10:57 +0200

    🚧 Create a random home page to keep the flashcard current state

---
+++ client/lib/pages/home/home.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flashxp/widgets/flashcard-mcq/flashcard_mcq.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var question = 'What is the spanish word for apple?';
+    var options = ['Manzana', 'Naranja', 'Plátano', 'Fresa'];
+    var correctOption = 'Manzana';
+
+    void getNextQuestion() {}
+
+    return Scaffold(
+      body: Center(
+        child: FlashcardMcq(
+          question: question,
+          options: options,
+          correctOption: correctOption,
+          onNext: getNextQuestion,
+        ),
+      ),
+    );
+  }
+}
