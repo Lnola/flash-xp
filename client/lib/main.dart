565e93132276932399febd6bf19278d7f8b341aa
Lnola <nola.luka@gmail.com>
2025-05-13 23:12:14 +0200

    🔥 Remove home page content from the main component

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/material.dart';
 
-import 'widgets/flashcard-mcq/flashcard_mcq.dart';
-
 void main() {
   runApp(const MainApp());
 }
@@ -11,28 +9,13 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    var question = 'What is the spanish word for apple?';
-    var options = ['Manzana', 'Naranja', 'Plátano', 'Fresa'];
-    var correctOption = 'Manzana';
-
-    void getNextQuestion() {}
-
     return MaterialApp(
       title: "flash-xp",
       theme: ThemeData(
         useMaterial3: true,
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue),
       ),
-      home: Scaffold(
-        body: Center(
-          child: FlashcardMcq(
-            question: question,
-            options: options,
-            correctOption: correctOption,
-            onNext: getNextQuestion,
-          ),
-        ),
-      ),
+      home: Scaffold(),
     );
   }
 }
