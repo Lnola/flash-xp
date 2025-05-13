32e6c40c00cc543c5ebf2b20682dcdf4a29ae7c5
Lnola <nola.luka@gmail.com>
2025-05-14 00:35:34 +0200

    🍱 Create an app color scheme

---
+++ client/lib/theme/app_theme.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  // Neutrals
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF120F11);
+  static const Color surface = Color(0xFFEAEDEF);
+
+  // Brand Colors
+  static const Color yellow = Color(0xFFFCE8AE);
+  static const Color blue = Color(0xFF98BFF5);
+  static const Color green = Color(0xFFCCEAB3);
+  static const Color purple = Color(0xFFC3A8FC);
+  static const Color red = Color(0xFFF5A7A4);
+}
+
+class AppTheme {
+  static ThemeData get light => ThemeData(
+        useMaterial3: true,
+        scaffoldBackgroundColor: AppColors.surface,
+        colorScheme: ColorScheme(
+          brightness: Brightness.light,
+          primary: AppColors.blue,
+          onPrimary: AppColors.black,
+          secondary: AppColors.purple,
+          onSecondary: AppColors.black,
+          surface: AppColors.white,
+          onSurface: AppColors.black,
+          error: AppColors.red,
+          onError: AppColors.white,
+        ),
+      );
+}
