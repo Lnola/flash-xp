6d4e79c19641fbc5972ecab14ae6d3a7a4eaacc3
Lnola <nola.luka@gmail.com>
2025-05-14 05:35:09 +0200

    🚧 Create a tabblable and nestable routing app

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,13 +1,5 @@
-import 'package:flashxp/state/navigation_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flashxp/theme/app_theme.dart';
-import 'package:flashxp/widgets/layout/fx_app_layout.dart';
-
-import 'package:flashxp/pages/home/home_page.dart';
-import 'package:flashxp/pages/explore/explore_page.dart';
-import 'package:flashxp/pages/create/create_page.dart';
-import 'package:flashxp/pages/statistics/statistics_page.dart';
-import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MainApp());
@@ -21,10 +13,7 @@ class MainApp extends StatelessWidget {
     return MaterialApp(
       title: "flash-xp",
       theme: AppTheme.light,
-      home: ChangeNotifierProvider(
-        create: (_) => NavigationState(),
-        child: const MainScaffold(),
-      ),
+      home: MainScaffold(),
     );
   }
 }
@@ -37,23 +26,80 @@ class MainScaffold extends StatefulWidget {
 }
 
 class _MainScaffoldState extends State<MainScaffold> {
-  final List<Widget> _pages = const [
-    HomePage(),
-    ExplorePage(),
-    CreatePage(),
-    StatisticsPage(),
+  int _currentIndex = 0;
+
+  final List<Widget> _tabs = const [
+    TabNavigator(index: 0),
+    TabNavigator(index: 1),
+    TabNavigator(index: 2),
+    TabNavigator(index: 3),
   ];
+
+  final List<GlobalKey<NavigatorState>> _navKeys = List.generate(
+    4,
+    (_) => GlobalKey<NavigatorState>(),
+  );
+
+  void _onTap(int index) {
+    if (index == _currentIndex) {
+      _navKeys[index].currentState?.popUntil((route) => route.isFirst);
+    } else {
+      setState(() => _currentIndex = index);
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    final navigationState = context.watch<NavigationState>();
-    final tabIndex = navigationState.tabIndex;
+    return Scaffold(
+      body: IndexedStack(
+        index: _currentIndex,
+        children: List.generate(
+          _tabs.length,
+          (i) => Navigator(
+            key: _navKeys[i],
+            onGenerateRoute: (_) => MaterialPageRoute(
+              builder: (_) => _tabs[i],
+            ),
+          ),
+        ),
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        currentIndex: _currentIndex,
+        onTap: _onTap,
+        items: const [
+          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Explore'),
+          BottomNavigationBarItem(icon: Icon(Icons.add), label: 'Create'),
+          BottomNavigationBarItem(icon: Icon(Icons.bar_chart), label: 'Stats'),
+        ],
+      ),
+    );
+  }
+}
 
-    return FxAppLayout(
-      showNavigation: true,
-      child: IndexedStack(
-        index: tabIndex,
-        children: _pages,
+class TabNavigator extends StatelessWidget {
+  final int index;
+  const TabNavigator({super.key, required this.index});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Tab $index')),
+      body: Center(
+        child: ElevatedButton(
+          child: const Text('Push new page'),
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (_) => Scaffold(
+                  appBar: AppBar(title: const Text('Nested Page')),
+                  body: const Center(child: Text('You pushed a new page!')),
+                ),
+              ),
+            );
+          },
+        ),
       ),
     );
   }
