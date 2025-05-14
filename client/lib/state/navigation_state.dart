c2153d86cec211f18a9c2aaa70c7288ae8f5960f
Lnola <nola.luka@gmail.com>
2025-05-14 03:49:31 +0200

    ✨ Add tab index getter and setter to the navigation state

--- client/lib/state/navigation_state.dart
+++ client/lib/state/navigation_state.dart
@@ -2,9 +2,15 @@ import 'package:flutter/material.dart';
 
 class NavigationState extends ChangeNotifier {
   var title = "Home";
+  var tabIndex = 0;
 
   void setTitle(String title) {
     title = title;
+    notifyListeners();
+  }
+
+  void setTabIndex(int index) {
+    tabIndex = index;
     notifyListeners();
   }
 }
