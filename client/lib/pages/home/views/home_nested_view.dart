e95dd14952f5beb2c6dca1df6771b2b64d6163e0
Lnola <nola.luka@gmail.com>
2025-06-04 00:15:43 +0200

    ♻️ Add the practice view as a nested home view

--- client/lib/pages/home/views/home_nested_view.dart
+++ client/lib/pages/home/views/home_nested_view.dart
@@ -1,15 +1,24 @@
-import 'package:flashxp/pages/home/views/home_view.dart';
-import 'package:flashxp/widgets/layout/flash_layout.dart';
+import 'package:flashxp/widgets/flashcard-mcq/flashcard_mcq.dart';
 import 'package:flutter/material.dart';
 
-class HomeNestedView extends StatelessWidget {
-  const HomeNestedView({super.key});
+class PracticeView extends StatelessWidget {
+  const PracticeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const FlashLayout(
-      title: 'Home Nested Page',
-      body: HomeView(),
+    final question = 'What is the spanish word for apple?';
+    final options = ['Manzana', 'Naranja', 'Plátano', 'Fresa'];
+    final correctOption = 'Manzana';
+
+    void getNextQuestion() {}
+
+    return Center(
+      child: FlashcardMcq(
+        question: question,
+        options: options,
+        correctOption: correctOption,
+        onNext: getNextQuestion,
+      ),
     );
   }
 }
