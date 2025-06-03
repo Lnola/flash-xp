d7bdd4b914391cefb3831b136a179aa20a6e7441
Lnola <nola.luka@gmail.com>
2025-06-03 17:13:57 +0200

    ♻️ Extract the bottom navigation bar into a custom class

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -25,6 +25,38 @@ class MainScaffold extends StatefulWidget {
   State<MainScaffold> createState() => _MainScaffoldState();
 }
 
+class BottomNavBar extends StatelessWidget {
+  final int currentIndex;
+  final void Function(int) onTap;
+
+  const BottomNavBar({
+    super.key,
+    required this.currentIndex,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      currentIndex: currentIndex,
+      onTap: onTap,
+      type: BottomNavigationBarType.fixed,
+      items: const [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.search),
+          label: 'Explore',
+        ),
+        BottomNavigationBarItem(icon: Icon(Icons.add), label: 'Create'),
+        BottomNavigationBarItem(
+          icon: Icon(Icons.bar_chart),
+          label: 'Stats',
+        ),
+      ],
+    );
+  }
+}
+
 class _MainScaffoldState extends State<MainScaffold> {
   int _currentIndex = 0;
 
@@ -63,16 +95,9 @@ class _MainScaffoldState extends State<MainScaffold> {
           ),
         ),
       ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
+      bottomNavigationBar: BottomNavBar(
         currentIndex: _currentIndex,
         onTap: _onTap,
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
-          BottomNavigationBarItem(icon: Icon(Icons.search), label: 'Explore'),
-          BottomNavigationBarItem(icon: Icon(Icons.add), label: 'Create'),
-          BottomNavigationBarItem(icon: Icon(Icons.bar_chart), label: 'Stats'),
-        ],
       ),
     );
   }
