8242cddb26353c71fe8ef0603650660d2f1d44ad
Lnola <nola.luka@gmail.com>
2025-06-04 01:01:19 +0200

    🚸 Create a not found widget

---
+++ client/lib/widgets/layout/flash_not_found.dart
@@ -0,0 +1,33 @@
+import 'package:flashxp/widgets/common/flash_button.dart';
+import 'package:flashxp/widgets/layout/flash_layout.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+class FlashNotFound extends StatelessWidget {
+  const FlashNotFound({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return FlashLayout(
+      title: 'Not Found',
+      body: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              Text(
+                'Oops! Page not found.',
+                style: Theme.of(context).textTheme.titleLarge,
+              ),
+              const SizedBox(height: 16),
+              FlashButton(
+                onPressed: () => context.go('/home'),
+                label: 'Go to Home',
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
