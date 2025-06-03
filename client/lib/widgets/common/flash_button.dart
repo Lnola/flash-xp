ca4514f2e11c60e87c6c7602b7ba9d9a38346c1e
Lnola <nola.luka@gmail.com>
2025-06-04 01:00:29 +0200

    ✨ Create a common button widget

---
+++ client/lib/widgets/common/flash_button.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+class FlashButton extends StatelessWidget {
+  final String label;
+  final VoidCallback onPressed;
+
+  const FlashButton({
+    super.key,
+    required this.label,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      style: ElevatedButton.styleFrom(
+        elevation: 0,
+        backgroundColor: Theme.of(context).colorScheme.primaryContainer,
+        foregroundColor: Theme.of(context).colorScheme.onPrimaryContainer,
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+        shape: const StadiumBorder(),
+        textStyle: Theme.of(context).textTheme.bodySmall,
+      ),
+      child: const Text('Start now'),
+    );
+  }
+}
