09b8c05c2ac591ee0912c102e61439eb498405af
Lnola <nola.luka@gmail.com>
2025-06-03 17:59:43 +0200

    🎨 Sort imports by package

--- client/lib/main.dart
+++ client/lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flashxp/theme/app_theme.dart';
 import 'package:go_router/go_router.dart';
 
+import 'package:flashxp/theme/app_theme.dart';
 import 'package:flashxp/router.dart';
 
 void main() {
