f010f0dfba41e84c4ae4c0b60fb2a318a75c77fc
Lnola <nola.luka@gmail.com>
2025-06-05 00:21:33 +0200

    💄 Add a vertical padding to the practice view

--- client/lib/pages/home/views/practice_view.dart
+++ client/lib/pages/home/views/practice_view.dart
@@ -14,20 +14,23 @@ class PracticeView extends StatelessWidget {
     final answer =
         'Answer is: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
 
-    return Column(
-      children: [
-        const PracticeProgress(current: 3, total: 12),
-        const SizedBox(height: 16),
-        PracticeQuestion(question: question, answer: answer),
-        const SizedBox(height: 24),
-        const PracticeOptionList(),
-        const SizedBox(height: 44),
-        FlashButton(
-          onPressed: () {},
-          label: 'Next Question',
-          isBlock: true,
-        ),
-      ],
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 8),
+      child: Column(
+        children: [
+          const PracticeProgress(current: 3, total: 12),
+          const SizedBox(height: 16),
+          PracticeQuestion(question: question, answer: answer),
+          const SizedBox(height: 24),
+          const PracticeOptionList(),
+          const SizedBox(height: 44),
+          FlashButton(
+            onPressed: () {},
+            label: 'Next Question',
+            isBlock: true,
+          ),
+        ],
+      ),
     );
   }
 }
