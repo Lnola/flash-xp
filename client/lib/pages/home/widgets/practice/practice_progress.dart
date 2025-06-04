1855bf5eda7f1c3c5edbfa4134f7f29cd54b4f70
Lnola <nola.luka@gmail.com>
2025-06-05 00:18:37 +0200

    ✨ Create a practice progress widget

---
+++ client/lib/pages/home/widgets/practice/practice_progress.dart
@@ -0,0 +1,30 @@
+import 'package:flashxp/widgets/common/flash_progress_bar.dart';
+import 'package:flutter/material.dart';
+
+class PracticeProgress extends StatelessWidget {
+  final int current;
+  final int total;
+
+  const PracticeProgress({
+    super.key,
+    required this.current,
+    required this.total,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final progress = current / total;
+
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.end,
+      children: [
+        FlashProgressBar(progress: progress),
+        const SizedBox(height: 4),
+        Text(
+          '$current of $total',
+          style: Theme.of(context).textTheme.titleSmall,
+        ),
+      ],
+    );
+  }
+}
