5d783cd5898e8037b646d3240032669dbc109cfa
Lnola <nola.luka@gmail.com>
2025-06-04 12:49:06 +0200

    ✨ Add background color as a prop for the flash layout

--- client/lib/widgets/layout/flash_layout.dart
+++ client/lib/widgets/layout/flash_layout.dart
@@ -3,16 +3,19 @@ import 'package:flutter/material.dart';
 class FlashLayout extends StatelessWidget {
   final String title;
   final Widget body;
+  final Color? backgroundColor;
 
   const FlashLayout({
     super.key,
     required this.title,
     required this.body,
+    this.backgroundColor,
   });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: backgroundColor,
       appBar: AppBar(title: Text(title)),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 22, vertical: 16),
