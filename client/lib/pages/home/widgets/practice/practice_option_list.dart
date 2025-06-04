8177ee76fde5f0fd03eab080216416fd0dbbe6e3
Lnola <nola.luka@gmail.com>
2025-06-05 00:30:23 +0200

    ♻️ Make practice option list dynamic

--- client/lib/pages/home/widgets/practice/practice_option_list.dart
+++ client/lib/pages/home/widgets/practice/practice_option_list.dart
@@ -1,32 +1,39 @@
 import 'package:flashxp/pages/home/widgets/practice/practice_option_button.dart';
 import 'package:flutter/material.dart';
 
+class OptionButtonData {
+  final String label;
+  final VoidCallback onPressed;
+  final PracticeOptionState state;
+
+  OptionButtonData({
+    required this.label,
+    required this.onPressed,
+    required this.state,
+  });
+}
+
 class PracticeOptionList extends StatelessWidget {
-  const PracticeOptionList({super.key});
+  final List<OptionButtonData> options;
+
+  const PracticeOptionList({super.key, required this.options});
 
   @override
   Widget build(BuildContext context) {
-    void onPressed() {}
-
     return Center(
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          Expanded(
-            child: PracticeOptionButton(
-              label: "I don't know",
-              onPressed: onPressed,
-              state: PracticeOptionState.incorrect,
+          for (int i = 0; i < options.length; i++) ...[
+            if (i != 0) const SizedBox(width: 8),
+            Expanded(
+              child: PracticeOptionButton(
+                label: options[i].label,
+                onPressed: options[i].onPressed,
+                state: options[i].state,
+              ),
             ),
-          ),
-          const SizedBox(width: 8),
-          Expanded(
-            child: PracticeOptionButton(
-              label: 'I know',
-              onPressed: onPressed,
-              state: PracticeOptionState.correct,
-            ),
-          ),
+          ],
         ],
       ),
     );
