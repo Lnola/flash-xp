ba5b4b806a31ef113cae1d900e8324f9add8d83a
Lnola <nola.luka@gmail.com>
2025-06-05 21:32:06 +0200

    ♻️ Split practice view so it fits the new structure

---
+++ client/lib/features/practice/data/answer_option_dto.dart
@@ -0,0 +1,9 @@
+class AnswerOption {
+  final String label;
+  final bool isCorrect;
+
+  AnswerOption({
+    required this.label,
+    this.isCorrect = false,
+  });
+}
--- client/lib/features/practice/presentation/practice_view.dart
+++ client/lib/features/practice/presentation/practice_view.dart
@@ -1,111 +1,11 @@
-import 'package:flashxp/features/practice/presentation/widgets/practice_option_button.dart';
+import 'package:flashxp/features/practice/data/answer_option_dto.dart';
+import 'package:flashxp/features/practice/logic/practice_mode_strategy.dart';
 import 'package:flashxp/features/practice/presentation/widgets/practice_option_list.dart';
 import 'package:flashxp/features/practice/presentation/widgets/practice_progress.dart';
 import 'package:flashxp/features/practice/presentation/widgets/practice_question.dart';
 import 'package:flashxp/widgets/common/flash_button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
-
-class AnswerOption {
-  final String label;
-  final bool isCorrect;
-
-  AnswerOption({
-    required this.label,
-    this.isCorrect = false,
-  });
-}
-
-enum PracticeMode { selfAssessment, multipleChoice }
-
-abstract class PracticeModeStrategy {
-  List<OptionButtonData> createOptions({
-    required void Function(String label) onPressed,
-    List<AnswerOption>? answerOptions,
-  });
-  List<OptionButtonData> updateOptions({
-    required String label,
-    required List<OptionButtonData> options,
-  });
-}
-
-class SelfAssessmentStrategy implements PracticeModeStrategy {
-  @override
-  List<OptionButtonData> createOptions({
-    required void Function(String label) onPressed,
-    List<AnswerOption>? answerOptions,
-  }) {
-    return [
-      OptionButtonData(
-        label: "I don't know",
-        onPressed: () => onPressed("I don't know"),
-        state: PracticeOptionState.incorrect,
-      ),
-      OptionButtonData(
-        label: 'I know',
-        onPressed: () => onPressed('I know'),
-        state: PracticeOptionState.correct,
-      ),
-    ];
-  }
-
-  @override
-  List<OptionButtonData> updateOptions({
-    required String label,
-    required List<OptionButtonData> options,
-  }) {
-    return options;
-  }
-}
-
-class MultipleChoiceStrategy implements PracticeModeStrategy {
-  static const correctLabel = 'C';
-
-  @override
-  List<OptionButtonData> createOptions({
-    required void Function(String label) onPressed,
-    List<AnswerOption>? answerOptions,
-  }) {
-    if (answerOptions == null || answerOptions.isEmpty) {
-      throw ArgumentError(
-        'MultipleChoiceStrategy options cannot be null or empty',
-      );
-    }
-    return _mapOptionsToOptionButtons(
-      answerOptions: answerOptions,
-      onPressed: (label) => onPressed(label),
-    );
-  }
-
-  @override
-  List<OptionButtonData> updateOptions({
-    required String label,
-    required List<OptionButtonData> options,
-  }) {
-    return options.map((option) {
-      var state = PracticeOptionState.defaultState;
-      if (option.isCorrect) {
-        state = PracticeOptionState.correct;
-      } else if (option.label == label) {
-        state = PracticeOptionState.incorrect;
-      }
-      return option.copyWith(state: state, isDisabled: true);
-    }).toList();
-  }
-
-  List<OptionButtonData> _mapOptionsToOptionButtons({
-    required List<AnswerOption> answerOptions,
-    required void Function(String label) onPressed,
-  }) {
-    return answerOptions.map((option) {
-      return OptionButtonData(
-        label: option.label,
-        isCorrect: option.isCorrect,
-        onPressed: () => onPressed(option.label),
-      );
-    }).toList();
-  }
-}
 
 class PracticeView extends StatefulWidget {
   const PracticeView({super.key});
