86babaf4870697879d6f7d6372d95c70bb999f46
Lnola <nola.luka@gmail.com>
2025-06-06 01:48:07 +0200

    🤡 Update the way questions are mocked

--- client/lib/features/practice/data/question.repository.dart
+++ client/lib/features/practice/data/question.repository.dart
@@ -4,13 +4,21 @@ import 'package:flashxp/features/practice/data/dto/question.dto.dart';
 class QuestionRepository {
   Future<List<QuestionDto>> fetch() async {
     await Future.delayed(const Duration(milliseconds: 500));
+    return _mockQuestions();
+  }
+
+  List<QuestionDto> _mockQuestions() {
+    final random = DateTime.now().millisecondsSinceEpoch % 2;
+    if (random == 0) return _mockMultipleChoiceQuestions();
+    return _mockSelfAssessmentQuestions();
+  }
+
+  List<QuestionDto> _mockMultipleChoiceQuestions() {
     return [
       QuestionDto(
         id: 1,
         text:
             'Question is: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        answer:
-            'Answer is: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
         answerOptionDtos: [
           AnswerOptionDto(label: 'A', isCorrect: true),
           AnswerOptionDto(label: 'B', isCorrect: false),
@@ -22,7 +30,6 @@ class QuestionRepository {
         id: 2,
         text:
             'Question is: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        answer: 'Answer is: Short',
         answerOptionDtos: [
           AnswerOptionDto(label: 'A', isCorrect: false),
           AnswerOptionDto(label: 'B', isCorrect: true),
@@ -53,16 +60,34 @@ class QuestionRepository {
           AnswerOptionDto(label: 'D', isCorrect: true),
         ],
       ),
+    ];
+  }
+
+  List<QuestionDto> _mockSelfAssessmentQuestions() {
+    return [
       QuestionDto(
-        id: 5,
+        id: 1,
         text:
             'Question is: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        answerOptionDtos: [],
+        answer:
+            'Answer is: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
       ),
       QuestionDto(
-        id: 5,
+        id: 2,
+        text:
+            'Question is: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        answer: 'Answer is: Short',
+      ),
+      QuestionDto(
+        id: 3,
         text: 'Question is: Short',
-        answerOptionDtos: [],
+        answer:
+            'Answer is: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      ),
+      QuestionDto(
+        id: 4,
+        text: 'Question is: Short',
+        answer: 'Answer is: Short',
       ),
     ];
   }
