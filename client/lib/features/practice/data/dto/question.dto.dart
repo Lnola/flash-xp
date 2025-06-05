d7bf69f6d484fc9666549749ece5bc31a71b46ed
Lnola <nola.luka@gmail.com>
2025-06-06 01:05:14 +0200

    ✨ Create a QuestionDto class

---
+++ client/lib/features/practice/data/dto/question.dto.dart
@@ -0,0 +1,15 @@
+import 'package:flashxp/features/practice/data/dto/answer_option.dto.dart';
+
+class QuestionDto {
+  final int id;
+  final String text;
+  final String? answer;
+  final List<AnswerOptionDto> answerOptionDtos;
+
+  QuestionDto({
+    required this.id,
+    required this.text,
+    this.answer,
+    required this.answerOptionDtos,
+  });
+}
