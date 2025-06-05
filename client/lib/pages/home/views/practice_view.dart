d44aca0c6323187b2f5a13113b0bc8bdcb7671da
Lnola <nola.luka@gmail.com>
2025-06-05 14:10:39 +0200

    🧑‍💻 Implement the strategy pattern to abstract logic for question types and modes

--- client/lib/pages/home/views/practice_view.dart
+++ client/lib/pages/home/views/practice_view.dart
@@ -8,30 +8,46 @@ import 'package:flutter_animate/flutter_animate.dart';
 
 enum PracticeMode { selfAssessment, multipleChoice }
 
-// TODO: clean up this factory and clicking logic
-class OptionButtonFactory {
-  static List<OptionButtonData> create({
-    required PracticeMode mode,
-    required void Function(String label) onPressed,
-  }) {
-    if (mode == PracticeMode.selfAssessment) {
-      return [
-        OptionButtonData(
-          label: "I don't know",
-          onPressed: () => onPressed("I don't know"),
-          state: PracticeOptionState.incorrect,
-        ),
-        OptionButtonData(
-          label: 'I know',
-          onPressed: () => onPressed('I know'),
-          state: PracticeOptionState.correct,
-        ),
-      ];
-    }
+abstract class PracticeModeStrategy {
+  List<OptionButtonData> createOptions(void Function(String label) onPressed);
+  void handleOptionPressed({
+    required String selectedLabel,
+    required List<OptionButtonData> options,
+    required void Function(List<OptionButtonData>) updateOptions,
+  });
+}
 
+class SelfAssessmentStrategy implements PracticeModeStrategy {
+  @override
+  List<OptionButtonData> createOptions(void Function(String label) onPressed) {
+    return [
+      OptionButtonData(
+        label: "I don't know",
+        onPressed: () => onPressed("I don't know"),
+        state: PracticeOptionState.incorrect,
+      ),
+      OptionButtonData(
+        label: 'I know',
+        onPressed: () => onPressed('I know'),
+        state: PracticeOptionState.correct,
+      ),
+    ];
+  }
+
+  @override
+  void handleOptionPressed({
+    required String selectedLabel,
+    required List<OptionButtonData> options,
+    required void Function(List<OptionButtonData>) updateOptions,
+  }) {}
+}
+
+class MultipleChoiceStrategy implements PracticeModeStrategy {
+  static const correctLabel = 'C';
+
+  @override
+  List<OptionButtonData> createOptions(void Function(String label) onPressed) {
     final labels = ['A', 'B', 'C', 'D'];
-    const correctLabel = 'C';
-
     return labels.map((label) {
       final isCorrect = label == correctLabel;
       return OptionButtonData(
@@ -40,6 +56,25 @@ class OptionButtonFactory {
         onPressed: () => onPressed(label),
       );
     }).toList();
+  }
+
+  @override
+  void handleOptionPressed({
+    required String selectedLabel,
+    required List<OptionButtonData> options,
+    required void Function(List<OptionButtonData>) updateOptions,
+  }) {
+    final updated = options.map((option) {
+      var state = PracticeOptionState.defaultState;
+      if (option.isCorrect) {
+        state = PracticeOptionState.correct;
+      } else if (option.label == selectedLabel) {
+        state = PracticeOptionState.incorrect;
+      }
+      return option.copyWith(state: state, isDisabled: true);
+    }).toList();
+
+    updateOptions(updated);
   }
 }
 
@@ -52,37 +87,27 @@ class PracticeView extends StatefulWidget {
 
 class _PracticeViewState extends State<PracticeView> {
   bool _hasAnswered = false;
-  PracticeMode mode = PracticeMode.multipleChoice;
+  late PracticeModeStrategy modeStrategy;
   late List<OptionButtonData> options;
-
-  void handleOptionPressed(String label) {
-    setState(() {
-      options = options.map((option) {
-        var state = PracticeOptionState.defaultState;
-        if (option.isCorrect) {
-          state = PracticeOptionState.correct;
-        } else if (option.label == label) {
-          state = PracticeOptionState.incorrect;
-        }
-        return option.copyWith(state: state, isDisabled: true);
-      }).toList();
-    });
-  }
 
   void initQuestion() {
     _hasAnswered = false;
-    options = OptionButtonFactory.create(
-      mode: mode,
-      onPressed: (label) {
+    options = modeStrategy.createOptions((label) {
+      setState(() {
         _hasAnswered = true;
-        if (mode == PracticeMode.multipleChoice) handleOptionPressed(label);
-      },
-    );
+        modeStrategy.handleOptionPressed(
+          selectedLabel: label,
+          options: options,
+          updateOptions: (updated) => options = updated,
+        );
+      });
+    });
   }
 
   @override
   void initState() {
     super.initState();
+    modeStrategy = MultipleChoiceStrategy();
     initQuestion();
   }
 
