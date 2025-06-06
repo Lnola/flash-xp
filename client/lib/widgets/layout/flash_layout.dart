829e5f1dfeb1b68dcb408d394cf0cb4d00b1b03b
Lnola <nola.luka@gmail.com>
2025-06-06 03:10:16 +0200

    ✨ Allow app to change the flash layout app bar color

--- client/lib/widgets/layout/flash_layout.dart
+++ client/lib/widgets/layout/flash_layout.dart
@@ -4,19 +4,24 @@ class FlashLayout extends StatelessWidget {
   final String title;
   final Widget body;
   final Color? backgroundColor;
+  final Color? appBarColor;
 
   const FlashLayout({
     super.key,
     required this.title,
     required this.body,
     this.backgroundColor,
+    this.appBarColor,
   });
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: backgroundColor,
-      appBar: AppBar(title: Text(title)),
+      appBar: AppBar(
+        title: Text(title),
+        backgroundColor: appBarColor,
+      ),
       body: Padding(
         padding: const EdgeInsets.symmetric(horizontal: 22, vertical: 16),
         child: body,
