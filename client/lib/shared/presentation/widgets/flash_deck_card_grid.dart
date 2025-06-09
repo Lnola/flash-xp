aaec2536bdf6936bbff6ac209638d3aaaa54f627
Lnola <nola.luka@gmail.com>
2025-06-09 16:36:16 +0200

    ✨ Add a title to the grid

--- client/lib/shared/presentation/widgets/flash_deck_card_grid.dart
+++ client/lib/shared/presentation/widgets/flash_deck_card_grid.dart
@@ -3,28 +3,37 @@ import 'package:flashxp/shared/presentation/widgets/flash_deck_card.dart';
 import 'package:flutter/material.dart';
 
 class FlashDeckCardGrid extends StatelessWidget {
+  final String title;
   final List<DeckDto> decks;
 
   const FlashDeckCardGrid({
     super.key,
+    required this.title,
     required this.decks,
   });
 
   @override
   Widget build(BuildContext context) {
-    return Wrap(
-      spacing: 12,
-      runSpacing: 12,
-      children: decks
-          .map(
-            (deck) => FlashDeckCard(
-              title: deck.title,
-              totalQuestions: deck.totalQuestions,
-              progress: deck.progress,
-              mode: deck.mode,
-            ),
-          )
-          .toList(),
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(title, style: Theme.of(context).textTheme.titleLarge),
+        const SizedBox(height: 16),
+        Wrap(
+          spacing: 12,
+          runSpacing: 12,
+          children: decks
+              .map(
+                (deck) => FlashDeckCard(
+                  title: deck.title,
+                  totalQuestions: deck.totalQuestions,
+                  progress: deck.progress,
+                  mode: deck.mode,
+                ),
+              )
+              .toList(),
+        ),
+      ],
     );
   }
 }
