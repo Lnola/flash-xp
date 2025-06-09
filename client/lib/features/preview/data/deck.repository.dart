ca40faa4ccb4cf6ab147d0a6e04cd88cc2b83d7f
Lnola <nola.luka@gmail.com>
2025-06-09 21:52:00 +0200

    ✨ Create a deck repository in the preview context

---
+++ client/lib/features/preview/data/deck.repository.dart
@@ -0,0 +1,37 @@
+import 'package:flashxp/features/preview/data/dto/deck.dto.dart';
+
+class DeckRepository {
+  Future<DeckDto> fetch() async {
+    await Future.delayed(const Duration(milliseconds: 500));
+    return _mockDeck();
+  }
+
+  DeckDto _mockDeck() {
+    return DeckDto(
+      id: 1,
+      title: 'Deck 1',
+      description:
+          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+      questions: [
+        Question(
+          id: 1,
+          text:
+              'Question is: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        ),
+        Question(
+          id: 2,
+          text:
+              'Question is: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+        ),
+        Question(
+          id: 3,
+          text: 'Question is: Short',
+        ),
+        Question(
+          id: 4,
+          text: 'Question is: Short',
+        ),
+      ],
+    );
+  }
+}
