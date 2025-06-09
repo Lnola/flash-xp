9523de7806fb47f5af4b0ccba3bcb8e3d6fc4073
Lnola <nola.luka@gmail.com>
2025-06-09 21:15:58 +0200

    ✨ Create a basic explore controller to init the decks

---
+++ client/lib/features/explore/logic/explore.controller.dart
@@ -0,0 +1,30 @@
+import 'package:flashxp/features/explore/data/deck.repository.dart';
+import 'package:flashxp/shared/data/dto/deck.dto.dart';
+import 'package:flutter/material.dart';
+
+class ExploreController extends ChangeNotifier {
+  final DeckRepository _deckRepository;
+
+  List<DeckDto> multipleChoiceDecks = [];
+  List<DeckDto> selfAssessmentDecks = [];
+  List<DeckDto> popularDecks = [];
+  List<DeckDto> forYouDecks = [];
+  bool isLoading = true;
+
+  ExploreController(this._deckRepository) {
+    _initDecks();
+  }
+
+  Future<void> _initDecks() async {
+    isLoading = true;
+    notifyListeners();
+
+    multipleChoiceDecks = await _deckRepository.fetch();
+    selfAssessmentDecks = await _deckRepository.fetch();
+    popularDecks = await _deckRepository.fetch();
+    forYouDecks = await _deckRepository.fetch();
+
+    isLoading = false;
+    notifyListeners();
+  }
+}
