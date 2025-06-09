8656700090ba9584aa490ed3bbb70456827adc83
Lnola <nola.luka@gmail.com>
2025-06-09 21:13:42 +0200

    💡 Leave a todo to move the dto to the use locations

--- client/lib/shared/data/dto/deck.dto.dart
+++ client/lib/shared/data/dto/deck.dto.dart
@@ -1,5 +1,6 @@
 import 'package:flashxp/shared/logic/domain/practice_mode.enum.dart';
 
+// TODO: Update this DTO to match all different uses of it in the app
 class DeckDto {
   final int id;
   final String title;
