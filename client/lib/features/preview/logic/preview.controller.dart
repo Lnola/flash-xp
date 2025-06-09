b9f708d4275d28fdffd93a68f49d97fc2228e39a
Lnola <nola.luka@gmail.com>
2025-06-09 22:31:29 +0200

    ✨ Expose deck questions from the controller

--- client/lib/features/preview/logic/preview.controller.dart
+++ client/lib/features/preview/logic/preview.controller.dart
@@ -1,4 +1,5 @@
 import 'package:flashxp/features/preview/data/deck.repository.dart';
+import 'package:flashxp/features/preview/data/dto/deck.dto.dart';
 import 'package:flutter/material.dart';
 
 class PreviewController extends ChangeNotifier {
@@ -6,6 +7,7 @@ class PreviewController extends ChangeNotifier {
 
   late String title;
   late String description;
+  late List<Question> questions;
   bool isLoading = true;
 
   PreviewController(this._deckRepository) {
@@ -18,6 +20,7 @@ class PreviewController extends ChangeNotifier {
     final deck = await _deckRepository.fetch();
     title = deck.title;
     description = deck.description;
+    questions = deck.questions;
 
     isLoading = false;
     notifyListeners();
