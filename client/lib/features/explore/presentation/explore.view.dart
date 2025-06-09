235fa86133abf6ec4d1b793c2a7cb1c0d5cbb385
Lnola <nola.luka@gmail.com>
2025-06-09 21:17:43 +0200

    ✨ Add deck grids to the explore view

--- client/lib/features/explore/presentation/explore.view.dart
+++ client/lib/features/explore/presentation/explore.view.dart
@@ -1,19 +1,62 @@
+import 'package:flashxp/features/explore/data/deck.repository.dart';
+import 'package:flashxp/features/explore/logic/explore.controller.dart';
+import 'package:flashxp/shared/presentation/widgets/flash_deck_card_grid.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 
-class ExploreView extends StatelessWidget {
+class ExploreView extends StatefulWidget {
   const ExploreView({super.key});
 
   @override
-  Widget build(BuildContext context) {
-    final currentPath = GoRouterState.of(context).uri.toString();
+  State<ExploreView> createState() => ExploreViewState();
+}
 
-    return Center(
-      child: ElevatedButton(
-        child: const Text('Push new page'),
-        onPressed: () {
-          context.push('$currentPath/nested');
-        },
+class ExploreViewState extends State<ExploreView> {
+  late final ExploreController controller;
+
+  void _onControllerUpdated() => setState(() {});
+
+  @override
+  void initState() {
+    super.initState();
+    controller = ExploreController(DeckRepository());
+    controller.addListener(_onControllerUpdated);
+  }
+
+  @override
+  void dispose() {
+    controller.removeListener(_onControllerUpdated);
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      clipBehavior: Clip.none,
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        spacing: 32,
+        children: [
+          FlashDeckCardGrid(
+            title: 'Multiple choice',
+            decks: controller.multipleChoiceDecks,
+            isLoading: controller.isLoading,
+          ),
+          FlashDeckCardGrid(
+            title: 'Flashcards',
+            decks: controller.selfAssessmentDecks,
+            isLoading: controller.isLoading,
+          ),
+          FlashDeckCardGrid(
+            title: 'Popular',
+            decks: controller.popularDecks,
+            isLoading: controller.isLoading,
+          ),
+          FlashDeckCardGrid(
+            title: 'For You',
+            decks: controller.forYouDecks,
+            isLoading: controller.isLoading,
+          ),
+        ],
       ),
     );
   }
