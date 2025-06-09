6defa593c135b505a2bda1afa4eaa9b991f7b9ea
Lnola <nola.luka@gmail.com>
2025-06-09 15:54:57 +0200

    ✨ Add deck as a param passed from the parent of the flash_card_grid

--- client/lib/shared/widgets/common/flash_card_grid.dart
+++ client/lib/shared/widgets/common/flash_card_grid.dart
@@ -1,36 +1,40 @@
+import 'package:flashxp/shared/dto/deck.dto.dart';
 import 'package:flashxp/shared/widgets/common/flash_card.dart';
 import 'package:flutter/material.dart';
 
 class FlashCardGrid extends StatelessWidget {
+  final DeckDto deck;
+
   const FlashCardGrid({
     super.key,
+    required this.deck,
   });
 
   @override
   Widget build(BuildContext context) {
-    return const Wrap(
+    return Wrap(
       spacing: 12,
       runSpacing: 12,
       children: [
         FlashCard(
-          title: 'Long title lorem ipsum dolor sit amet',
-          totalQuestions: 20,
-          progress: 50,
-          mode: FlashCardMode.multipleChoice,
+          title: deck.title,
+          totalQuestions: deck.totalQuestions,
+          progress: deck.progress,
+          mode: deck.mode,
         ),
-        FlashCard(
+        const FlashCard(
           title: 'Short title',
           totalQuestions: 12,
           progress: 10,
           mode: FlashCardMode.multipleChoice,
         ),
-        FlashCard(
+        const FlashCard(
           title: 'Short title',
           totalQuestions: 40,
           progress: 40,
           mode: FlashCardMode.selfAssessment,
         ),
-        FlashCard(
+        const FlashCard(
           title: 'Long title lorem ipsum dolor sit amet',
           totalQuestions: 100,
           progress: 100,
