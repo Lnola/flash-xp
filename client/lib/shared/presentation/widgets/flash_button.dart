b2caba0f9374c0fc724311d658b9bc566e4cdb6f
Lnola <nola.luka@gmail.com>
2025-06-10 00:25:02 +0200

    💄 Create a secondary variant button

--- client/lib/shared/presentation/widgets/flash_button.dart
+++ client/lib/shared/presentation/widgets/flash_button.dart
@@ -4,27 +4,42 @@ class FlashButton extends StatelessWidget {
   final String label;
   final VoidCallback onPressed;
   final bool isBlock;
+  final bool isSecondary;
 
   const FlashButton({
     super.key,
     required this.label,
     required this.onPressed,
     this.isBlock = false,
+    this.isSecondary = false,
   });
 
   @override
   Widget build(BuildContext context) {
+    final backgroundColor = isSecondary
+        ? Colors.transparent
+        : Theme.of(context).colorScheme.primaryContainer;
+    final foregroundColor = isSecondary
+        ? Theme.of(context).colorScheme.primaryContainer
+        : Theme.of(context).colorScheme.onPrimaryContainer;
+
     final button = ElevatedButton(
       onPressed: onPressed,
       style: ElevatedButton.styleFrom(
         elevation: 0,
-        backgroundColor: Theme.of(context).colorScheme.primaryContainer,
-        foregroundColor: Theme.of(context).colorScheme.onPrimaryContainer,
+        backgroundColor: backgroundColor,
+        foregroundColor: foregroundColor,
         padding: EdgeInsets.symmetric(
           horizontal: 12,
           vertical: isBlock ? 16 : 8,
         ),
-        shape: const StadiumBorder(),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(100),
+          side: BorderSide(
+            color: Theme.of(context).colorScheme.primaryContainer,
+            width: 2,
+          ),
+        ),
         textStyle: Theme.of(context).textTheme.labelMedium,
       ),
       child: Text(label),
