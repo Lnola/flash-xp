f5ab3d7fdd2942afc8bdc55656a2120d68b3a7b6
Lnola <nola.luka@gmail.com>
2025-06-09 18:20:28 +0200

    💩 Add hardcoded displays for swiper and grid

--- client/lib/features/home/presentation/home.view.dart
+++ client/lib/features/home/presentation/home.view.dart
@@ -1,19 +1,59 @@
+import 'package:flashxp/shared/data/dto/deck.dto.dart';
+import 'package:flashxp/shared/logic/domain/practice_mode.enum.dart';
+import 'package:flashxp/shared/presentation/widgets/flash_deck_card_grid.dart';
+import 'package:flashxp/shared/presentation/widgets/flash_deck_card_swiper.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final currentPath = GoRouterState.of(context).uri.toString();
+    final decks = [
+      DeckDto(
+        id: 1,
+        title: 'Long title lorem ipsum dolor sit amet',
+        totalQuestions: 4,
+        progress: 25,
+        mode: PracticeMode.multipleChoice,
+      ),
+      DeckDto(
+        id: 2,
+        title: 'Short title',
+        totalQuestions: 12,
+        progress: 10,
+        mode: PracticeMode.multipleChoice,
+      ),
+      DeckDto(
+        id: 3,
+        title: 'Short title',
+        totalQuestions: 40,
+        progress: 40,
+        mode: PracticeMode.selfAssessment,
+      ),
+      DeckDto(
+        id: 4,
+        title: 'Long title lorem ipsum dolor sit amet',
+        totalQuestions: 100,
+        progress: 100,
+        mode: PracticeMode.selfAssessment,
+      ),
+    ];
 
-    return Center(
-      child: ElevatedButton(
-        child: const Text('Push new page'),
-        onPressed: () {
-          context.push('$currentPath/practice');
-        },
+    return SingleChildScrollView(
+      clipBehavior: Clip.none,
+      child: Column(
+        children: [
+          FlashDeckCardSwiper(
+            title: 'Swiper',
+            decks: decks,
+          ),
+          const SizedBox(height: 32),
+          FlashDeckCardGrid(
+            title: 'Grid',
+            decks: decks,
+          ),
+        ],
       ),
     );
   }
