56ea3219f2e1482e7cd4ae4c72e97f64980535af
Lnola <nola.luka@gmail.com>
2025-06-09 19:22:17 +0200

    ♻️ Leverage the deck repository for fetching decks

--- client/lib/features/home/logic/home.controller.dart
+++ client/lib/features/home/logic/home.controller.dart
@@ -1,45 +1,24 @@
+import 'package:flashxp/features/home/data/deck.repository.dart';
 import 'package:flashxp/shared/data/dto/deck.dto.dart';
-import 'package:flashxp/shared/logic/domain/practice_mode.enum.dart';
 import 'package:flutter/material.dart';
 
 class HomeController extends ChangeNotifier {
-  List<DeckDto> decks = [];
+  final DeckRepository _deckRepository;
 
-  HomeController() {
-    _fetchDecks();
+  List<DeckDto> decks = [];
+  bool isLoading = true;
+
+  HomeController(this._deckRepository) {
+    _initDecks();
   }
 
-  void _fetchDecks() {
-    decks = [
-      DeckDto(
-        id: 1,
-        title: 'Long title lorem ipsum dolor sit amet',
-        totalQuestions: 4,
-        progress: 25,
-        mode: PracticeMode.multipleChoice,
-      ),
-      DeckDto(
-        id: 2,
-        title: 'Short title',
-        totalQuestions: 12,
-        progress: 10,
-        mode: PracticeMode.multipleChoice,
-      ),
-      DeckDto(
-        id: 3,
-        title: 'Short title',
-        totalQuestions: 40,
-        progress: 40,
-        mode: PracticeMode.selfAssessment,
-      ),
-      DeckDto(
-        id: 4,
-        title: 'Long title lorem ipsum dolor sit amet',
-        totalQuestions: 100,
-        progress: 100,
-        mode: PracticeMode.selfAssessment,
-      ),
-    ];
+  Future<void> _initDecks() async {
+    isLoading = true;
+    notifyListeners();
+
+    decks = await _deckRepository.fetch();
+
+    isLoading = false;
     notifyListeners();
   }
 }
