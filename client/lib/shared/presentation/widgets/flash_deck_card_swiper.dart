53843f26d68e00e048cf9650595a1d212fff8f90
Lnola <nola.luka@gmail.com>
2025-06-09 21:23:46 +0200

    🚸 Add a skeleton loader to the card swiper

--- client/lib/shared/presentation/widgets/flash_deck_card_swiper.dart
+++ client/lib/shared/presentation/widgets/flash_deck_card_swiper.dart
@@ -1,21 +1,28 @@
 import 'package:flashxp/shared/data/dto/deck.dto.dart';
 import 'package:flashxp/shared/presentation/widgets/flash_deck_card.dart';
+import 'package:flashxp/shared/presentation/widgets/flash_deck_card_skeleton.dart';
 import 'package:flutter/material.dart';
 
 class FlashDeckCardSwiper extends StatelessWidget {
   final String title;
   final Color? backgroundColor;
   final List<DeckDto> decks;
+  final bool isLoading;
 
   const FlashDeckCardSwiper({
     super.key,
     required this.title,
     this.backgroundColor,
     required this.decks,
+    this.isLoading = false,
   });
 
   @override
   Widget build(BuildContext context) {
+    if (decks.isEmpty && !isLoading) {
+      return const SizedBox.shrink();
+    }
+
     return LayoutBuilder(
       builder: (context, constraints) => Column(
         crossAxisAlignment: CrossAxisAlignment.start,
@@ -26,6 +33,7 @@ class FlashDeckCardSwiper extends StatelessWidget {
             decks: decks,
             backgroundColor: backgroundColor,
             cardWidth: (constraints.maxWidth / 2) - 6,
+            isLoading: isLoading,
           ),
         ],
       ),
@@ -37,35 +45,44 @@ class _SwiperLayout extends StatelessWidget {
   final List<DeckDto> decks;
   final Color? backgroundColor;
   final double cardWidth;
+  final bool isLoading;
 
   const _SwiperLayout({
     required this.decks,
     required this.backgroundColor,
     required this.cardWidth,
+    required this.isLoading,
   });
 
   @override
   Widget build(BuildContext context) {
+    final cards = isLoading
+        ? _buildDeckCardsSkeleton(cardWidth)
+        : _buildDeckCards(cardWidth);
+
     return SingleChildScrollView(
       scrollDirection: Axis.horizontal,
       clipBehavior: Clip.none,
-      child: Row(
-        children: decks
-            .map(
-              (deck) => Padding(
-                padding: const EdgeInsets.only(right: 12),
-                child: FlashDeckCard(
-                  title: deck.title,
-                  totalQuestions: deck.totalQuestions,
-                  progress: deck.progress,
-                  mode: deck.mode,
-                  backgroundColor: backgroundColor,
-                  width: cardWidth,
-                ),
-              ),
-            )
-            .toList(),
-      ),
+      child: Row(spacing: 12, children: cards),
     );
+  }
+
+  List<Widget> _buildDeckCardsSkeleton(double cardWidth) {
+    return List.generate(2, (_) => FlashDeckCardSkeleton(width: cardWidth));
+  }
+
+  List<Widget> _buildDeckCards(double cardWidth) {
+    return decks
+        .map(
+          (deck) => FlashDeckCard(
+            title: deck.title,
+            totalQuestions: deck.totalQuestions,
+            progress: deck.progress,
+            mode: deck.mode,
+            backgroundColor: backgroundColor,
+            width: cardWidth,
+          ),
+        )
+        .toList();
   }
 }
