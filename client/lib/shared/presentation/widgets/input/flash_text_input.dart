238eb156f97dcc834d4c5383e7c71a375cc11acc
Lnola <nola.luka@gmail.com>
2025-06-11 02:25:29 +0200

    ♻️ Add color change and text field focus on tap by adding stateful isFocused

--- client/lib/shared/presentation/widgets/input/flash_text_input.dart
+++ client/lib/shared/presentation/widgets/input/flash_text_input.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class FlashTextInput extends StatelessWidget {
+class FlashTextInput extends StatefulWidget {
   final String label;
   final TextEditingController controller;
 
@@ -11,54 +11,78 @@ class FlashTextInput extends StatelessWidget {
   });
 
   @override
+  State<FlashTextInput> createState() => _FlashTextInputState();
+}
+
+class _FlashTextInputState extends State<FlashTextInput> {
+  late final FocusNode _focusNode;
+  bool _isFocused = false;
+
+  @override
+  void initState() {
+    super.initState();
+    _focusNode = FocusNode();
+    _focusNode.addListener(_handleFocusChange);
+  }
+
+  @override
+  void dispose() {
+    _focusNode.removeListener(_handleFocusChange);
+    _focusNode.dispose();
+    super.dispose();
+  }
+
+  void _handleFocusChange() {
+    if (mounted) {
+      setState(() {
+        _isFocused = _focusNode.hasFocus;
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
 
-    return Focus(
-      child: Builder(
-        builder: (context) {
-          final isFocused = Focus.of(context).hasFocus;
-          return GestureDetector(
-            onTap: () => FocusScope.of(context).requestFocus(Focus.of(context)),
-            child: Container(
-              width: double.infinity,
-              decoration: BoxDecoration(
-                color: theme.colorScheme.surface,
-                borderRadius: BorderRadius.circular(16),
-                border: Border.all(
-                  color: isFocused
-                      ? theme.colorScheme.primary
-                      : theme.colorScheme.outline.withAlpha(77),
-                  width: 1,
-                ),
-              ),
-              padding: const EdgeInsets.all(12),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    label,
-                    style: theme.textTheme.bodySmall?.copyWith(
-                      color: theme.colorScheme.onSurface.withAlpha(77),
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                  TextField(
-                    controller: controller,
-                    keyboardType: TextInputType.multiline,
-                    maxLines: null,
-                    decoration: const InputDecoration(
-                      border: InputBorder.none,
-                      isDense: true,
-                      contentPadding: EdgeInsets.zero,
-                    ),
-                    style: theme.textTheme.bodyLarge,
-                  ),
-                ],
+    return GestureDetector(
+      onTap: () => _focusNode.requestFocus(),
+      child: Container(
+        width: double.infinity,
+        padding: const EdgeInsets.all(12),
+        decoration: BoxDecoration(
+          color: theme.colorScheme.surface,
+          borderRadius: BorderRadius.circular(16),
+          border: Border.all(
+            color: _isFocused
+                ? theme.colorScheme.primary
+                : theme.colorScheme.outline.withAlpha(77),
+            width: 1,
+          ),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              widget.label,
+              style: theme.textTheme.bodySmall?.copyWith(
+                color: theme.colorScheme.onSurface.withAlpha(77),
+                fontWeight: FontWeight.w600,
               ),
             ),
-          );
-        },
+            TextField(
+              controller: widget.controller,
+              focusNode: _focusNode,
+              keyboardType: TextInputType.multiline,
+              maxLines: null,
+              decoration: const InputDecoration(
+                border: InputBorder.none,
+                isDense: true,
+                contentPadding: EdgeInsets.zero,
+              ),
+              style: theme.textTheme.bodyLarge,
+            ),
+          ],
+        ),
       ),
     );
   }
