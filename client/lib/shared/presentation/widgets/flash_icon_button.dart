2f2f6acd446cf3fa38081c3ae52a5d8341accb49
Lnola <nola.luka@gmail.com>
2025-06-11 03:16:44 +0200

    ✨ Create an icon button component

---
+++ client/lib/shared/presentation/widgets/flash_icon_button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class FlashIconButton extends StatelessWidget {
+  final VoidCallback onPressed;
+  final String label;
+  final IconData icon;
+
+  const FlashIconButton({
+    super.key,
+    required this.onPressed,
+    required this.label,
+    required this.icon,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return FloatingActionButton.extended(
+      onPressed: onPressed,
+      label: Text(
+        label,
+        style: Theme.of(context).textTheme.titleSmall?.copyWith(
+              color: Theme.of(context).colorScheme.onPrimaryContainer,
+            ),
+      ),
+      icon: FaIcon(
+        icon,
+        size: 16,
+      ),
+      elevation: 0,
+    );
+  }
+}
