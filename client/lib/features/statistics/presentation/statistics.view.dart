60134563009ea94638b547cc31b01ab78c39131a
Lnola <nola.luka@gmail.com>
2025-06-20 17:57:12 +0200

    🚚 Move the sign out button to the statistics page

--- client/lib/features/statistics/presentation/statistics.view.dart
+++ client/lib/features/statistics/presentation/statistics.view.dart
@@ -1,19 +1,23 @@
+import 'package:flashxp/shared/logic/service/auth.service.dart';
+import 'package:flashxp/shared/presentation/widgets/flash_button.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
 
 class StatisticsView extends StatelessWidget {
   const StatisticsView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    final currentPath = GoRouterState.of(context).uri.toString();
-
+    final authService = context.read<AuthService>();
     return Center(
-      child: ElevatedButton(
-        child: const Text('Push new page'),
-        onPressed: () {
-          context.push('$currentPath/nested');
+      child: FlashButton(
+        onPressed: () async {
+          await authService.signOut();
+          // TODO: Handle sign out properly
+          context.go('/auth');
         },
+        label: 'Sign Out',
       ),
     );
   }
