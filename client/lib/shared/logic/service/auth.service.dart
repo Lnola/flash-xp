3bf64239b14da541e354fda84a5007024d7934c5
Lnola <nola.luka@gmail.com>
2025-06-20 16:48:43 +0200

    🛂 Add the auth service

---
+++ client/lib/shared/logic/service/auth.service.dart
@@ -0,0 +1,47 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class AuthService with ChangeNotifier {
+  final _auth = FirebaseAuth.instance;
+
+  User? get user => _auth.currentUser;
+
+  bool get isSignedIn => user != null;
+
+  Future<void> authenticate(String email, String password) async {
+    try {
+      await _signIn(email, password);
+    } on FirebaseAuthException catch (e) {
+      if (e.code == 'invalid-credential') {
+        try {
+          await _register(email, password);
+        } on FirebaseAuthException catch (_) {
+          rethrow;
+        }
+      } else {
+        rethrow;
+      }
+    }
+  }
+
+  Future<void> _signIn(String email, String password) async {
+    await _auth.signInWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    notifyListeners();
+  }
+
+  Future<void> _register(String email, String password) async {
+    await _auth.createUserWithEmailAndPassword(
+      email: email,
+      password: password,
+    );
+    notifyListeners();
+  }
+
+  Future<void> signOut() async {
+    await _auth.signOut();
+    notifyListeners();
+  }
+}
