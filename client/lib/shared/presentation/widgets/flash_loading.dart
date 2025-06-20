e73b00e0dc1cf61adcc8aaa5bf639807885a553f
Lnola <nola.luka@gmail.com>
2025-06-20 17:54:40 +0200

    ✨ Make the loading spinner adjustable

--- client/lib/shared/presentation/widgets/flash_loading.dart
+++ client/lib/shared/presentation/widgets/flash_loading.dart
@@ -1,12 +1,25 @@
 import 'package:flutter/material.dart';
 
 class FlashLoading extends StatelessWidget {
-  const FlashLoading({super.key});
+  final double? height;
+  final double? width;
+  final Color? color;
+
+  const FlashLoading({
+    super.key,
+    this.height,
+    this.width,
+    this.color,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return CircularProgressIndicator(
-      color: Theme.of(context).colorScheme.onSurface,
+    return SizedBox(
+      height: height ?? 40,
+      width: width ?? 40,
+      child: CircularProgressIndicator(
+        color: color ?? Theme.of(context).colorScheme.onSurface,
+      ),
     );
   }
 }
