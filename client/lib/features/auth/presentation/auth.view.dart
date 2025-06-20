6f8f741139dd717a733098b93d0fd898f7792775
Lnola <nola.luka@gmail.com>
2025-06-20 16:39:41 +0200

    ✨ Create a basic auth view

---
+++ client/lib/features/auth/presentation/auth.view.dart
@@ -0,0 +1,56 @@
+import 'package:flashxp/shared/logic/service/auth.service.dart';
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
+
+class AuthView extends StatefulWidget {
+  const AuthView({super.key});
+
+  @override
+  State<AuthView> createState() => _AuthViewState();
+}
+
+class _AuthViewState extends State<AuthView> {
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+  String? error;
+
+  void _login() async {
+    try {
+      final authService = context.read<AuthService>();
+      await authService.authenticate(
+        emailController.text,
+        passwordController.text,
+      );
+      context.go('/home');
+    } catch (e) {
+      print(e);
+      setState(() => error = 'Unable to authenticate. Please try again.');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            TextField(
+              controller: emailController,
+              decoration: const InputDecoration(labelText: 'Email'),
+            ),
+            TextField(
+              controller: passwordController,
+              decoration: const InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            if (error != null)
+              Text(error!, style: const TextStyle(color: Colors.red)),
+            ElevatedButton(onPressed: _login, child: const Text('Login')),
+          ],
+        ),
+      ),
+    );
+  }
+}
