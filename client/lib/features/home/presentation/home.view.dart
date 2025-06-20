562c0c54de54699070047ab8a50e57da8ae28045
Lnola <nola.luka@gmail.com>
2025-06-20 17:24:39 +0200

    💩 Add a temporary signout button to the top of home page

--- client/lib/features/home/presentation/home.view.dart
+++ client/lib/features/home/presentation/home.view.dart
@@ -1,7 +1,11 @@
 import 'package:flashxp/features/home/data/deck.repository.dart';
 import 'package:flashxp/features/home/logic/home.controller.dart';
+import 'package:flashxp/shared/logic/service/auth.service.dart';
+import 'package:flashxp/shared/presentation/widgets/flash_button.dart';
 import 'package:flashxp/shared/presentation/widgets/flash_deck_card_grid.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -30,12 +34,21 @@ class _HomeViewState extends State<HomeView> {
 
   @override
   Widget build(BuildContext context) {
+    final authService = context.read<AuthService>();
     return SingleChildScrollView(
       clipBehavior: Clip.none,
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         spacing: 32,
         children: [
+          FlashButton(
+            onPressed: () async {
+              await authService.signOut();
+              // TODO: Handle sign out properly
+              context.go('/auth');
+            },
+            label: authService.user?.email ?? 'Sign Out',
+          ),
           FlashDeckCardGrid(
             title: 'In progress',
             decks: controller.inProgressDecks,
