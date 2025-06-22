c1ed0e61af9b072c4dbc99fa5d6298c1fa5770fb
Lnola <nola.luka@gmail.com>
2025-06-23 00:38:11 +0200

    ✨ Attach the CatalogModule to the AppModule

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
+import { CatalogModule } from 'catalog/catalog.module';
 import { DbConfigModule } from 'config/database.config';
 import { OrmConfigModule } from 'config/orm.config';
 
 @Module({
-  imports: [DbConfigModule, OrmConfigModule],
+  imports: [DbConfigModule, OrmConfigModule, CatalogModule],
 })
 export class AppModule {}
