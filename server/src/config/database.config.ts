24359c9bed50afa3fe172b83bf5667e0614ce4ef
Lnola <nola.luka@gmail.com>
2025-06-22 22:27:43 +0200

    🐛 Register the config object to include dotenv

--- server/src/config/database.config.ts
+++ server/src/config/database.config.ts
@@ -8,14 +8,14 @@ export type DatabaseConfig = {
   password?: string;
 };
 
-const databaseConfig: DatabaseConfig = {
+const databaseConfig = registerAs<DatabaseConfig>('database', () => ({
   port: parseInt(process.env.DB_PORT ?? '5432', 10),
   host: process.env.DB_HOST,
   dbName: process.env.DB_NAME,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
-};
+}));
 
 export const DbConfigModule = ConfigModule.forRoot({
-  load: [registerAs<DatabaseConfig>('database', () => databaseConfig)],
+  load: [databaseConfig],
 });
