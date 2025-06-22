48934cc40f42ebe4869c372189038daac65a7d4e
Lnola <nola.luka@gmail.com>
2025-06-22 22:38:55 +0200

    🔧 Add the orm and db config modules to the app module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,8 @@
 import { Module } from '@nestjs/common';
+import { DbConfigModule } from 'config/database.config';
+import { OrmConfigModule } from 'config/orm.config';
 
 @Module({
-  imports: [],
+  imports: [DbConfigModule, OrmConfigModule],
 })
 export class AppModule {}
