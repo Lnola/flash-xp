04b307ca0d82f57644f9fa8ec8f6c6fe87bf53fe
Lnola <nola.luka@gmail.com>
2025-06-23 00:29:45 +0200

    🚚 Move the mikroorm config to the root

--- server/mikro-orm.config.ts
+++ server/mikro-orm.config.ts
@@ -1,6 +1,6 @@
 import * as dotenv from 'dotenv';
-import databaseConfigFactory from './config/database.config';
-import mikroOrmConfig from './config/orm.config';
+import databaseConfigFactory from 'config/database.config';
+import mikroOrmConfig from 'config/orm.config';
 import { defineConfig } from '@mikro-orm/core';
 
 dotenv.config();
