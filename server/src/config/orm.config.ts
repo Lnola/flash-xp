f94660075f124586bb16e28d7ab323bf1660f61e
Lnola <nola.luka@gmail.com>
2025-06-22 23:50:01 +0200

    🐛 Fix orm config

--- server/src/config/orm.config.ts
+++ server/src/config/orm.config.ts
@@ -4,23 +4,26 @@ import { PostgreSqlDriver } from '@mikro-orm/postgresql';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { DatabaseConfig } from 'config/database.config';
 
-const mikroOrmConfig = defineConfig({
+const mikroOrmConfig = {
   driver: PostgreSqlDriver,
   discovery: { checkDuplicateTableNames: false },
   migrations: {
     path: `${process.cwd()}/src/shared/database/migrations`,
     disableForeignKeys: false,
+    pattern: /^\d+[\w-]+\.ts$/,
+    fileName: (timestamp: string) => `${timestamp}-new-migration`,
   },
   debug: true,
-});
+  autoLoadEntities: true,
+};
 export default mikroOrmConfig;
 
 export const OrmConfigModule = MikroOrmModule.forRootAsync({
   imports: [ConfigModule],
   inject: [ConfigService],
-  useFactory: (config: ConfigService) => ({
-    ...config.get<DatabaseConfig>('database'),
-    ...mikroOrmConfig,
-    autoLoadEntities: true,
-  }),
+  useFactory: (config: ConfigService) =>
+    defineConfig({
+      ...config.get<DatabaseConfig>('database'),
+      ...mikroOrmConfig,
+    }),
 });
