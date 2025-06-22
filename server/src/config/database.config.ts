2d41ce65eaa7717ad5579ab9c177841601107f8a
Lnola <nola.luka@gmail.com>
2025-06-22 22:21:17 +0200

    🔧 Create a database config

---
+++ server/src/config/database.config.ts
@@ -0,0 +1,21 @@
+import { ConfigModule, registerAs } from '@nestjs/config';
+
+export type DatabaseConfig = {
+  port: number;
+  host?: string;
+  dbName?: string;
+  user?: string;
+  password?: string;
+};
+
+const databaseConfig: DatabaseConfig = {
+  port: parseInt(process.env.DB_PORT ?? '5432', 10),
+  host: process.env.DB_HOST,
+  dbName: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+};
+
+export const DbConfigModule = ConfigModule.forRoot({
+  load: [registerAs<DatabaseConfig>('database', () => databaseConfig)],
+});
