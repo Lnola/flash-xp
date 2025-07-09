86f68a240da17f5566b8112c8fc9b69b2e23a1b9
Lnola <nola.luka@gmail.com>
2025-07-09 22:12:42 +0200

    ⚰️ Remove the default export file

--- server/src/catalog/core/entities/index.ts
+++
@@ -1,5 +0,0 @@
-export { AnswerOption } from './answer-option.entity';
-export { Deck } from './deck.entity';
-export { Learner } from './learner.entity';
-export { QuestionType } from './question-type.entity';
-export { Question } from './question.entity';
