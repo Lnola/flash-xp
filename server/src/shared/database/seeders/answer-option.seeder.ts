f834ceeb881d05988540fbaba8d3b27afc3a114f
Lnola <nola.luka@gmail.com>
2025-07-09 21:51:38 +0200

    🌱 Update the random questions and answers to fit eachother

--- server/src/shared/database/seeders/answer-option.seeder.ts
+++ server/src/shared/database/seeders/answer-option.seeder.ts
@@ -4,46 +4,34 @@ import { AnswerOption } from 'catalog/core/entities';
 import { DatabaseSeederContext } from '.';
 
 const answerOptions = [
-  {
-    text: 'Paris',
-    isCorrect: false,
-    questionIndex: 0,
-  },
-  {
-    text: 'Berlin',
-    isCorrect: false,
-    questionIndex: 0,
-  },
-  {
-    text: 'Madrid',
-    isCorrect: false,
-    questionIndex: 0,
-  },
-  {
-    text: 'London',
-    isCorrect: false,
-    questionIndex: 0,
-  },
-  {
-    text: 'Einstein',
-    isCorrect: false,
-    questionIndex: 1,
-  },
-  {
-    text: 'Newton',
-    isCorrect: false,
-    questionIndex: 1,
-  },
-  {
-    text: 'Galileo',
-    isCorrect: false,
-    questionIndex: 1,
-  },
-  {
-    text: 'Curie',
-    isCorrect: false,
-    questionIndex: 1,
-  },
+  { text: 'Paris', isCorrect: true, questionIndex: 0 },
+  { text: 'Berlin', isCorrect: false, questionIndex: 0 },
+  { text: 'Madrid', isCorrect: false, questionIndex: 0 },
+  { text: 'London', isCorrect: false, questionIndex: 0 },
+  { text: 'Paris', isCorrect: false, questionIndex: 1 },
+  { text: 'Berlin', isCorrect: true, questionIndex: 1 },
+  { text: 'Madrid', isCorrect: false, questionIndex: 1 },
+  { text: 'London', isCorrect: false, questionIndex: 1 },
+  { text: 'Paris', isCorrect: false, questionIndex: 2 },
+  { text: 'Berlin', isCorrect: false, questionIndex: 2 },
+  { text: 'Madrid', isCorrect: true, questionIndex: 2 },
+  { text: 'London', isCorrect: false, questionIndex: 2 },
+  { text: 'Paris', isCorrect: false, questionIndex: 3 },
+  { text: 'Berlin', isCorrect: false, questionIndex: 3 },
+  { text: 'Madrid', isCorrect: false, questionIndex: 3 },
+  { text: 'London', isCorrect: true, questionIndex: 3 },
+  { text: 'Einstein', isCorrect: true, questionIndex: 8 },
+  { text: 'Newton', isCorrect: false, questionIndex: 8 },
+  { text: 'Galileo', isCorrect: false, questionIndex: 8 },
+  { text: 'Curie', isCorrect: false, questionIndex: 8 },
+  { text: 'Einstein', isCorrect: true, questionIndex: 9 },
+  { text: 'Newton', isCorrect: false, questionIndex: 9 },
+  { text: 'Galileo', isCorrect: false, questionIndex: 9 },
+  { text: 'Curie', isCorrect: false, questionIndex: 9 },
+  { text: 'Einstein', isCorrect: true, questionIndex: 10 },
+  { text: 'Newton', isCorrect: false, questionIndex: 10 },
+  { text: 'Galileo', isCorrect: false, questionIndex: 10 },
+  { text: 'Curie', isCorrect: false, questionIndex: 10 },
 ];
 
 export class AnswerOptionSeeder extends Seeder {
