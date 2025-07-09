2063636f11035e9cd4615512336590eeb30c4dee
Lnola <nola.luka@gmail.com>
2025-07-09 21:36:20 +0200

    🌱 Create a QuestionSeeder that seeds a couple of questions

---
+++ server/src/shared/database/seeders/question.seeder.ts
@@ -0,0 +1,40 @@
+import type { EntityManager } from '@mikro-orm/core';
+import { Seeder } from '@mikro-orm/seeder';
+import { Question } from 'catalog/core/entities';
+import { DatabaseSeederContext } from '.';
+
+const questions = [
+  {
+    text: 'What is the capital of France?',
+    deckIndex: 0,
+    questionTypeIndex: 0,
+  },
+  {
+    text: 'Explain the theory of relativity.',
+    deckIndex: 1,
+    questionTypeIndex: 1,
+  },
+  {
+    text: 'What is the largest mammal?',
+    deckIndex: 2,
+    questionTypeIndex: 0,
+  },
+  {
+    text: 'Describe the process of photosynthesis.',
+    deckIndex: 3,
+    questionTypeIndex: 1,
+  },
+];
+
+export class QuestionSeeder extends Seeder {
+  async run(em: EntityManager, context: DatabaseSeederContext): Promise<void> {
+    const seedQuestions = questions.map((it) => {
+      return new Question({
+        ...it,
+        deck: context.decks[it.deckIndex],
+        questionType: context.questionTypes[it.questionTypeIndex],
+      });
+    });
+    return em.persistAndFlush(seedQuestions);
+  }
+}
