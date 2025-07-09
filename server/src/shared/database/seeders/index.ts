b0da411ec0c8ebeeeddea576cb1cd4b36ec0f10a
Lnola <nola.luka@gmail.com>
2025-07-09 21:45:42 +0200

    🌱 Add the AnswerOptionSeeder to the DatabaseSeeder

--- server/src/shared/database/seeders/index.ts
+++ server/src/shared/database/seeders/index.ts
@@ -1,12 +1,19 @@
 import type { EntityManager } from '@mikro-orm/core';
 import { Seeder } from '@mikro-orm/seeder';
 import { Deck, Learner, Question, QuestionType } from 'catalog/core/entities';
+import { AnswerOptionSeeder } from './answer-option.seeder';
 import { DeckSeeder } from './deck.seeder';
 import { QuestionTypeSeeder } from './question-type.seeder';
 import { QuestionSeeder } from './question.seeder';
 import { UserSeeder } from './user.seeder';
 
-const seeders = [UserSeeder, DeckSeeder, QuestionTypeSeeder, QuestionSeeder];
+const seeders = [
+  UserSeeder,
+  DeckSeeder,
+  QuestionTypeSeeder,
+  QuestionSeeder,
+  AnswerOptionSeeder,
+];
 
 export class DatabaseSeeder extends Seeder {
   async run(em: EntityManager): Promise<void> {
