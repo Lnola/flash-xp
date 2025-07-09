3c5ea8ef0eef84af242e372ab4f53c8066a59ea2
Lnola <nola.luka@gmail.com>
2025-07-09 17:02:03 +0200

    🔧 Configure the seeder

--- server/mikro-orm.config.ts
+++ server/mikro-orm.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from '@mikro-orm/core';
+import { SeedManager } from '@mikro-orm/seeder';
 import * as dotenv from 'dotenv';
 import databaseConfigFactory from 'config/database.config';
 import { mikroOrmDriver } from 'config/orm.config';
@@ -13,5 +14,11 @@ export default defineConfig({
     disableForeignKeys: false,
     fileName: (timestamp: string) => `${timestamp}-new-migration`,
   },
+  seeder: {
+    path: `${process.cwd()}/src/shared/database/seeders`,
+    fileName: (className: string) =>
+      `${className.toLowerCase().replace('seeder', '.seeder')}`,
+  },
   entities: [`./**/entities/*.entity.ts`],
+  extensions: [SeedManager],
 });
