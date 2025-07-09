e8fd3f998db994c0426d7636ea378cefc54b9bfc
Lnola <nola.luka@gmail.com>
2025-07-09 21:35:47 +0200

    ✨ Add a constructor to the Question entity

--- server/src/catalog/core/entities/question.entity.ts
+++ server/src/catalog/core/entities/question.entity.ts
@@ -2,6 +2,12 @@ import { Entity, Property, ManyToOne } from '@mikro-orm/core';
 import BaseEntity from 'shared/database/base.entity';
 import { Deck, QuestionType } from '.';
 
+type CreateQuestionProps = {
+  text: string;
+  deck: Deck;
+  questionType: QuestionType;
+};
+
 @Entity({ tableName: 'question' })
 export class Question extends BaseEntity {
   @Property()
@@ -12,4 +18,11 @@ export class Question extends BaseEntity {
 
   @ManyToOne(() => QuestionType, { eager: true })
   questionType!: QuestionType;
+
+  constructor({ text, deck, questionType }: CreateQuestionProps) {
+    super();
+    this.text = text;
+    this.deck = deck;
+    this.questionType = questionType;
+  }
 }
