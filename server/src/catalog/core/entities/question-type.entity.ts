025867a3d7285efc3267984223a33759ca4c2482
Lnola <nola.luka@gmail.com>
2025-07-09 21:26:41 +0200

    ✨ Add a constructor to the question type entity

--- server/src/catalog/core/entities/question-type.entity.ts
+++ server/src/catalog/core/entities/question-type.entity.ts
@@ -1,8 +1,16 @@
 import { Entity, Property } from '@mikro-orm/core';
 import BaseEntity from 'shared/database/base.entity';
 
+type CreateQuestionTypeProps = {
+  name: string;
+};
 @Entity({ tableName: 'question_type' })
 export class QuestionType extends BaseEntity {
   @Property()
   name!: string;
+
+  constructor({ name }: CreateQuestionTypeProps) {
+    super();
+    this.name = name;
+  }
 }
