f12e46f78f1cd13f3daa69020d456f5a56bf8263
Lnola <nola.luka@gmail.com>
2025-07-09 16:49:35 +0200

    ✨ Create the answer option entity

---
+++ server/src/catalog/core/entities/answer-option.entity.ts
@@ -0,0 +1,15 @@
+import { Entity, ManyToOne, Property } from '@mikro-orm/core';
+import BaseEntity from 'shared/database/base.entity';
+import { Question } from './question.entity';
+
+@Entity({ tableName: 'answer_option' })
+export class AnswerOption extends BaseEntity {
+  @Property({ length: 120 })
+  text!: string;
+
+  @Property()
+  isCorrect!: boolean;
+
+  @ManyToOne(() => Question)
+  question!: Question;
+}
