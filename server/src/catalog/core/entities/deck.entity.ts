c01d20f249ab121308fea2094bd699a5b32ea2f4
Lnola <nola.luka@gmail.com>
2025-07-09 16:44:55 +0200

    ✨ Add questions to the deck entity

--- server/src/catalog/core/entities/deck.entity.ts
+++ server/src/catalog/core/entities/deck.entity.ts
@@ -1,5 +1,6 @@
-import { Entity, Property } from '@mikro-orm/core';
+import { Collection, Entity, OneToMany, Property } from '@mikro-orm/core';
 import BaseEntity from 'shared/database/base.entity';
+import { Question } from './question.entity';
 
 @Entity({ tableName: 'deck' })
 export class Deck extends BaseEntity {
@@ -8,4 +9,7 @@ export class Deck extends BaseEntity {
 
   @Property({ length: 300 })
   description!: string;
+
+  @OneToMany(() => Question, (question) => question.deck)
+  questions = new Collection<Question>(this);
 }
