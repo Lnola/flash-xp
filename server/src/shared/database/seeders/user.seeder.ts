a9df1f522575281e0ddd224492cc220495a37716
Lnola <nola.luka@gmail.com>
2025-07-09 18:07:08 +0200

    🌱 Create a user seeder that seeds 3 users

---
+++ server/src/shared/database/seeders/user.seeder.ts
@@ -0,0 +1,17 @@
+import type { EntityManager } from '@mikro-orm/core';
+import { Seeder } from '@mikro-orm/seeder';
+import { Learner } from 'catalog/core/entities/learner.entity';
+import { DatabaseSeederContext } from '.';
+
+const users = [{}, {}, {}];
+
+export class UserSeeder extends Seeder {
+  run(em: EntityManager, context: DatabaseSeederContext): Promise<void> {
+    const seedUsers = users.map(() => {
+      // TODO: update Learner to User once that is implemented
+      return new Learner();
+    });
+    context.users = seedUsers;
+    return em.persistAndFlush(seedUsers);
+  }
+}
