2eb0c187ff60e3743b1756159731d29ca1b51fc7
Lnola <nola.luka@gmail.com>
2025-07-09 18:05:44 +0200

    ✨ Add a constructor to the deck entity

--- server/src/catalog/core/entities/deck.entity.ts
+++ server/src/catalog/core/entities/deck.entity.ts
@@ -2,6 +2,11 @@ import { Collection, Entity, OneToMany, Property } from '@mikro-orm/core';
 import BaseEntity from 'shared/database/base.entity';
 import { Question } from './question.entity';
 
+type CreateDeckProps = {
+  title: string;
+  description: string;
+};
+
 @Entity({ tableName: 'deck' })
 export class Deck extends BaseEntity {
   @Property({ length: 50 })
@@ -12,4 +17,11 @@ export class Deck extends BaseEntity {
 
   @OneToMany(() => Question, (question) => question.deck)
   questions = new Collection<Question>(this);
+
+  constructor({ title, description }: CreateDeckProps) {
+    super();
+
+    this.title = title;
+    this.description = description;
+  }
 }
