6e24213bcadb300e2522b577cfa86994ef22b870
Lnola <nola.luka@gmail.com>
2025-07-09 22:13:28 +0200

    ✨ Add the catalog deck entity to the catalog bounded context

---
+++ server/src/catalog/core/entities/catalog-deck.entity.ts
@@ -0,0 +1,11 @@
+import { Entity, Property } from '@mikro-orm/core';
+import BaseEntity from 'shared/database/base.entity';
+
+@Entity({ tableName: 'deck' })
+export class CatalogDeck extends BaseEntity {
+  @Property()
+  title!: string;
+
+  @Property()
+  description!: string;
+}
---
+++ server/src/catalog/core/entities/index.ts
@@ -0,0 +1,1 @@
+export { CatalogDeck } from './catalog-deck.entity';
