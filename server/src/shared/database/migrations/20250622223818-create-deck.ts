31f924f56e0d916aeb5fd8a83f327baaa907755e
Lnola <nola.luka@gmail.com>
2025-07-09 16:02:07 +0200

    🗃️ Update the migration to use knex

--- server/src/shared/database/migrations/20250622223818-create-deck.ts
+++ server/src/shared/database/migrations/20250622223818-create-deck.ts
@@ -1,13 +1,23 @@
 import { Migration } from '@mikro-orm/migrations';
+import { createBaseEntity } from './queries/create-base-entity';
+
+const DECK_TABLE_NAME = 'deck';
 
 export class CreateDeck extends Migration {
   override up(): void {
-    this.addSql(
-      `create table "deck" ("id" serial primary key, "title" varchar(255) not null, "description" varchar(255) not null);`,
-    );
+    const knex = this.getKnex();
+    const createDeck = knex.schema.createTable(DECK_TABLE_NAME, (table) => {
+      createBaseEntity(table, knex);
+      table.string('title', 50).notNullable();
+      table.string('description', 300).notNullable();
+    });
+
+    this.addSql(createDeck.toQuery());
   }
 
   override down(): void {
-    this.addSql(`drop table if exists "deck" cascade;`);
+    const knex = this.getKnex();
+    const dropDeck = knex.schema.dropTable(DECK_TABLE_NAME);
+    this.addSql(dropDeck.toQuery());
   }
 }
