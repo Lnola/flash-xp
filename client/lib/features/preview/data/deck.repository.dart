19495c045499a1e86f228da2cce4159c48008027
Lnola <nola.luka@gmail.com>
2025-07-10 17:05:52 +0200

    🚧 Replace the mock fetch with a real one

--- client/lib/features/preview/data/deck.repository.dart
+++ client/lib/features/preview/data/deck.repository.dart
@@ -1,38 +1,21 @@
+import 'dart:convert';
+
 import 'package:flashxp/features/preview/data/dto/deck.dto.dart';
+import 'package:http/http.dart' as http;
 
 class DeckRepository {
-  Future<DeckDto> fetch() async {
-    await Future.delayed(const Duration(milliseconds: 500));
-    return _mockDeck();
+  Future<DeckDto> fetch(int id) async {
+    // TODO: improve the fetch itself
+    final response =
+        await http.get(Uri.parse('http://localhost:3000/decks/$id'));
+
+    // TODO: throw the correct error that comes from the server
+    if (response.statusCode != 200) throw Exception('Failed to fetch deck.');
+    return parseDeck(response.body);
   }
 
-  DeckDto _mockDeck() {
-    return DeckDto(
-      id: 1,
-      title: 'Deck 1',
-      description:
-          'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-      isBookmarked: false,
-      questions: [
-        Question(
-          id: 1,
-          text:
-              'Question is: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        ),
-        Question(
-          id: 2,
-          text:
-              'Question is: Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-        ),
-        Question(
-          id: 3,
-          text: 'Question is: Short',
-        ),
-        Question(
-          id: 4,
-          text: 'Question is: Short',
-        ),
-      ],
-    );
+  DeckDto parseDeck(String jsonString) {
+    final dynamic response = json.decode(jsonString);
+    return DeckDto.fromJson(response);
   }
 }
