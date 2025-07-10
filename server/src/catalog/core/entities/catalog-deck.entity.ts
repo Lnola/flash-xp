af7c7d5ff274028f45c97d6ba053fbd3d8216505
Lnola <nola.luka@gmail.com>
2025-07-10 17:04:07 +0200

    ✨ Add a relation between catalog decks and questions

--- server/src/catalog/core/entities/catalog-deck.entity.ts
+++ server/src/catalog/core/entities/catalog-deck.entity.ts
@@ -1,5 +1,6 @@
-import { Entity, Property } from '@mikro-orm/core';
+import { Collection, Entity, OneToMany, Property } from '@mikro-orm/core';
 import BaseEntity from 'shared/database/base.entity';
+import { CatalogQuestion } from './catalog-question.entity';
 
 @Entity({ tableName: 'deck' })
 export class CatalogDeck extends BaseEntity {
@@ -11,4 +12,7 @@ export class CatalogDeck extends BaseEntity {
 
   @Property()
   authorId!: number;
+
+  @OneToMany(() => CatalogQuestion, (question) => question.deck)
+  questions = new Collection<CatalogQuestion>(this);
 }
