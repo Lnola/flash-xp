7b57ee8509ed7dc7c29bfed44d15ff25a904d2a9
Lnola <nola.luka@gmail.com>
2025-07-12 13:29:06 +0200

    🗃️ Add a migration to create bookmark

---
+++ server/src/shared/database/migrations/20250712110911-create-bookmark.ts
@@ -0,0 +1,28 @@
+import { Migration } from '@mikro-orm/migrations';
+import { createBaseEntity } from './queries/create-base-entity';
+
+const BOOKMARK_TABLE_NAME = 'bookmark';
+
+export class CreateBookmark extends Migration {
+  override up(): void {
+    const knex = this.getKnex();
+    const createBookmark = knex.schema.createTable(
+      BOOKMARK_TABLE_NAME,
+      (table) => {
+        createBaseEntity(table, knex);
+        table.integer('user_id').notNullable();
+        table.foreign('user_id').references('user.id');
+        table.integer('deck_id').notNullable();
+        table.foreign('deck_id').references('deck.id');
+      },
+    );
+
+    this.addSql(createBookmark.toQuery());
+  }
+
+  override down(): void {
+    const knex = this.getKnex();
+    const dropBookmark = knex.schema.dropTable(BOOKMARK_TABLE_NAME);
+    this.addSql(dropBookmark.toQuery());
+  }
+}
