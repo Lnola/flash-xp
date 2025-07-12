ed8d55d712d4b6036c5c53f13fa06a6f0ed390ba
Lnola <nola.luka@gmail.com>
2025-07-13 01:15:33 +0200

    🛂 Provide the firebase auth guard globally

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { CatalogModule } from 'catalog/catalog.module';
 import { DbConfigModule } from 'config/database.config';
 import { OrmConfigModule } from 'config/orm.config';
+import { FirebaseAuthGuardProvider } from 'shared/guards';
 import { FirebaseProvider } from 'shared/providers';
 
 @Module({
   imports: [DbConfigModule, OrmConfigModule, CatalogModule],
-  providers: [FirebaseProvider],
+  providers: [FirebaseProvider, FirebaseAuthGuardProvider],
 })
 export class AppModule {}
