36ec74fc58589fd8d050b805f4379a0b6ad970f9
Lnola <nola.luka@gmail.com>
2025-07-12 14:48:48 +0200

    🗃️ Add a composite unique index on deckId and learnerId on the entity level

--- server/src/catalog/core/entities/bookmark.entity.ts
+++ server/src/catalog/core/entities/bookmark.entity.ts
@@ -1,8 +1,9 @@
-import { Entity, Property } from '@mikro-orm/core';
+import { Entity, Property, Unique } from '@mikro-orm/core';
 import BaseEntity from 'shared/database/base.entity';
 import { CatalogDeck, Learner } from '.';
 
 @Entity({ tableName: 'bookmark' })
+@Unique({ properties: ['deckId', 'learnerId'] })
 export class Bookmark extends BaseEntity {
   @Property()
   deckId!: CatalogDeck['id'];
