f4c9b01ae6988a07638e506ac104a6078887d847
Lnola <nola.luka@gmail.com>
2025-07-13 01:18:56 +0200

    🛂 Leverage the AuthHttpClient for api calls

--- client/lib/features/home/data/deck.repository.dart
+++ client/lib/features/home/data/deck.repository.dart
@@ -1,12 +1,14 @@
 import 'dart:convert';
 
+import 'package:flashxp/shared/data/auth_http_client.dart';
 import 'package:flashxp/shared/data/dto/deck.dto.dart';
-import 'package:http/http.dart' as http;
 
 class DeckRepository {
+  final client = AuthHttpClient();
+
   Future<List<DeckDto>> fetch() async {
     // TODO: improve the fetch itself
-    final response = await http.get(Uri.parse('http://localhost:3000/decks'));
+    final response = await client.get(Uri.parse('http://localhost:3000/decks'));
 
     // TODO: throw the correct error that comes from the server
     if (response.statusCode != 200) throw Exception('Failed to fetch decks.');
--- client/lib/features/preview/data/deck.repository.dart
+++ client/lib/features/preview/data/deck.repository.dart
@@ -1,13 +1,15 @@
 import 'dart:convert';
 
 import 'package:flashxp/features/preview/data/dto/deck.dto.dart';
-import 'package:http/http.dart' as http;
+import 'package:flashxp/shared/data/auth_http_client.dart';
 
 class DeckRepository {
+  final client = AuthHttpClient();
+
   Future<DeckDto> fetch(int id) async {
     // TODO: improve the fetch itself
     final response =
-        await http.get(Uri.parse('http://localhost:3000/decks/$id'));
+        await client.get(Uri.parse('http://localhost:3000/decks/$id'));
 
     // TODO: throw the correct error that comes from the server
     if (response.statusCode != 200) throw Exception('Failed to fetch deck.');
