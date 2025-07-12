1f4aaebe076d2eff259b88d1f825585d8d319263
Lnola <nola.luka@gmail.com>
2025-07-13 01:12:17 +0200

    ✨ Register the firebase provider globally

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,8 +2,10 @@ import { Module } from '@nestjs/common';
 import { CatalogModule } from 'catalog/catalog.module';
 import { DbConfigModule } from 'config/database.config';
 import { OrmConfigModule } from 'config/orm.config';
+import { FirebaseProvider } from 'shared/providers';
 
 @Module({
   imports: [DbConfigModule, OrmConfigModule, CatalogModule],
+  providers: [FirebaseProvider],
 })
 export class AppModule {}
