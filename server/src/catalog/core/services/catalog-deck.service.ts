49f230497ac577b75750bbdd6da2b4a29793500d
Lnola <nola.luka@gmail.com>
2025-07-12 14:45:23 +0200

    💩 Add create and delete bookmark methods to the service

--- server/src/catalog/core/services/catalog-deck.service.ts
+++ server/src/catalog/core/services/catalog-deck.service.ts
@@ -1,13 +1,15 @@
 import { InjectRepository } from '@mikro-orm/nestjs';
-import { EntityRepository } from '@mikro-orm/postgresql';
 import { Injectable } from '@nestjs/common';
-import { CatalogDeck } from 'catalog/core/entities';
+import { Bookmark, CatalogDeck, Learner } from 'catalog/core/entities';
+import { BaseEntityRepository } from 'shared/database/base.repository';
 
 @Injectable()
 export class CatalogDeckService {
   constructor(
     @InjectRepository(CatalogDeck)
-    private readonly catalogDeckRepository: EntityRepository<CatalogDeck>,
+    private readonly catalogDeckRepository: BaseEntityRepository<CatalogDeck>,
+    @InjectRepository(Bookmark)
+    private readonly bookmarkRepository: BaseEntityRepository<Bookmark>,
   ) {}
 
   fetchAll() {
@@ -19,5 +21,22 @@ export class CatalogDeckService {
     return this.catalogDeckRepository.findOne(deckId, {
       populate: ['questions'],
     });
+  }
+
+  // TODO: add error handling
+  async createBookmark(deckId: CatalogDeck['id'], learnerId: Learner['id']) {
+    console.log(deckId, learnerId);
+    const bookmark = new Bookmark(deckId, learnerId);
+    await this.bookmarkRepository.persistAndFlush(bookmark);
+  }
+
+  async deleteBookmark(deckId: CatalogDeck['id'], learnerId: Learner['id']) {
+    const bookmarkToRemove = await this.bookmarkRepository.findOne({
+      deckId: deckId,
+      learnerId: learnerId,
+    });
+    // TODO: add error handling
+    if (!bookmarkToRemove) throw new Error(`Not found`);
+    await this.bookmarkRepository.removeAndFlush(bookmarkToRemove);
   }
 }
