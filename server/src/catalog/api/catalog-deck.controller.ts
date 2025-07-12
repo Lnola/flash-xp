38dab6eeca312919c9d69ac187cedfdfb4d89fdc
Lnola <nola.luka@gmail.com>
2025-07-12 14:44:41 +0200

    ✨ Add create and delete bookmark methods

--- server/src/catalog/api/catalog-deck.controller.ts
+++ server/src/catalog/api/catalog-deck.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post } from '@nestjs/common';
 import { CatalogDeck } from 'catalog/core/entities';
 import { CatalogDeckService } from 'catalog/core/services';
+import { CreateBookmarkDto, DeleteBookmarkDto } from './dto';
 
 // TODO: think about the endpoint naming convention
 @Controller('decks')
@@ -15,5 +16,17 @@ export class CatalogDeckController {
   @Get(':id')
   fetchById(@Param('id') deckId: CatalogDeck['id']) {
     return this.catalogDeckService.fetchById(deckId);
+  }
+
+  // TODO: move to a separate controller
+  @Post('bookmarks')
+  createBookmark(@Body() { deckId, learnerId }: CreateBookmarkDto) {
+    return this.catalogDeckService.createBookmark(deckId, learnerId);
+  }
+
+  // TODO: move to a separate controller
+  @Delete('bookmarks')
+  deleteBookmark(@Body() { deckId, learnerId }: DeleteBookmarkDto) {
+    return this.catalogDeckService.deleteBookmark(deckId, learnerId);
   }
 }
