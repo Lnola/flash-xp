0a4b30d08217e5c17cc4099d7289d63ffb151869
Lnola <nola.luka@gmail.com>
2025-07-13 01:16:29 +0200

    🛂 Create an AuthHttpClient that intersects requests and adds the bearer token

---
+++ client/lib/shared/data/auth_http_client.dart
@@ -0,0 +1,14 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:http/http.dart' as http;
+
+class AuthHttpClient extends http.BaseClient {
+  final http.Client _inner = http.Client();
+
+  @override
+  Future<http.StreamedResponse> send(http.BaseRequest request) async {
+    final user = FirebaseAuth.instance.currentUser;
+    final idToken = await user?.getIdToken();
+    if (idToken != null) request.headers['Authorization'] = 'Bearer $idToken';
+    return _inner.send(request);
+  }
+}
