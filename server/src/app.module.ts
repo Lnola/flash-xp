32ac5d703d8bf51a6143abc6c9611eb283caeb77
Lnola <nola.luka@gmail.com>
2025-07-13 19:03:05 +0200

    ✨ Register the authoring module in the app.module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
+import { AuthoringModule } from 'authoring/authoring.module';
 import { CatalogModule } from 'catalog/catalog.module';
 import { AuthModule } from 'shared/auth/auth.module';
 import { DbConfigModule, OrmConfigModule } from 'shared/config';
 import { FirebaseProvider } from 'shared/providers';
 
 @Module({
-  imports: [DbConfigModule, OrmConfigModule, AuthModule, CatalogModule],
+  imports: [
+    DbConfigModule,
+    OrmConfigModule,
+    AuthModule,
+    CatalogModule,
+    AuthoringModule,
+  ],
   providers: [FirebaseProvider],
 })
 export class AppModule {}
