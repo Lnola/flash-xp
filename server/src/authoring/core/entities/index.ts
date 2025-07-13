415339023ec6d983d08621d0a76c0be81f2f7b08
Lnola <nola.luka@gmail.com>
2025-07-13 18:33:16 +0200

    🎨 Sort exports alphabetically

--- server/src/authoring/core/entities/index.ts
+++ server/src/authoring/core/entities/index.ts
@@ -1,5 +1,5 @@
 export { AnswerOption } from './answer-option.entity';
-export { Deck } from './deck.entity';
 export { Author } from './author.entity';
+export { Deck } from './deck.entity';
 export { QuestionType } from './question-type.entity';
 export { Question } from './question.entity';
