495e74c9097a9fd5985065fb145c6608cb213d76
Lnola <nola.luka@gmail.com>
2025-07-13 19:14:52 +0200

    🚧 Create a fetchById method on the deck service

--- server/src/authoring/core/services/deck.service.ts
+++ server/src/authoring/core/services/deck.service.ts
@@ -1,6 +1,19 @@
+import { InjectRepository } from '@mikro-orm/nestjs';
 import { Injectable } from '@nestjs/common';
+import { Deck } from 'authoring/core/entities';
+import { BaseEntityRepository } from 'shared/database/base.repository';
+import { Result } from 'shared/helpers/result';
 
 @Injectable()
 export class DeckService {
-  constructor() {}
+  constructor(
+    @InjectRepository(Deck)
+    private readonly deckRepository: BaseEntityRepository<Deck>,
+  ) {}
+
+  async fetchById(deckId: Deck['id']): Promise<Result<Deck>> {
+    const deck = await this.deckRepository.findOne(deckId);
+    if (!deck) return Result.failure('Deck not found');
+    return Result.success(deck);
+  }
 }
