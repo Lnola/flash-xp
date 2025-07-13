81b046790280547705ec2b3d02497c4df8409dd4
Lnola <nola.luka@gmail.com>
2025-07-13 17:31:12 +0200

    🛂 Add db user management to the auth guard

--- server/src/shared/guards/firebase-auth.guard.ts
+++ server/src/shared/guards/firebase-auth.guard.ts
@@ -1,3 +1,4 @@
+import { InjectRepository } from '@mikro-orm/nestjs';
 import {
   CanActivate,
   ExecutionContext,
@@ -7,9 +8,16 @@ import {
 import { APP_GUARD } from '@nestjs/core';
 import { Request } from 'express';
 import * as firebaseAdmin from 'firebase-admin';
+import { BaseEntityRepository } from 'shared/database/base.repository';
+import { User } from 'shared/entities';
 
 @Injectable()
 export class FirebaseAuthGuard implements CanActivate {
+  constructor(
+    @InjectRepository(User)
+    private readonly userRepository: BaseEntityRepository<User>,
+  ) {}
+
   async canActivate(context: ExecutionContext): Promise<boolean> {
     const request = context.switchToHttp().getRequest<Request>();
     const authHeader = request.headers.authorization;
@@ -20,6 +28,17 @@ export class FirebaseAuthGuard implements CanActivate {
       const idToken = authHeader.split('Bearer ')[1];
       const decodedToken = await firebaseAdmin.auth().verifyIdToken(idToken);
       request.ssoId = decodedToken.uid;
+
+      const user = await this.userRepository.findOne({
+        ssoId: decodedToken.uid,
+      });
+      if (user) {
+        request.user = user;
+        return true;
+      }
+      const newUser = new User({ ssoId: decodedToken.uid });
+      await this.userRepository.persistAndFlush(newUser);
+      request.user = newUser;
       return true;
     } catch {
       throw new UnauthorizedException('Failed to authenticate.');
