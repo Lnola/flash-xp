f571114ce53540d7440669418fbf5af640d8a4cb
Lnola <nola.luka@gmail.com>
2025-07-13 17:12:36 +0200

    🌱 Update the UserSeeder to leverage the User entity and ssoId

--- server/src/shared/database/seeders/user.seeder.ts
+++ server/src/shared/database/seeders/user.seeder.ts
@@ -1,15 +1,18 @@
 import type { EntityManager } from '@mikro-orm/core';
 import { Seeder } from '@mikro-orm/seeder';
-import { Author } from 'authoring/core/entities';
+import { User } from 'shared/entities';
 import { DatabaseSeederContext } from '.';
 
-const users = [{}, {}, {}];
+const users = [
+  { ssoId: 'FouvnrzjyIW6NP3AixejNinkUu02' },
+  { ssoId: 'nBDBh8BBc3QTiFZrEdkPipUNe8e2' },
+  { ssoId: 'uPNDSIdXplVwapNV4OAPGgLmLsj1' },
+];
 
 export class UserSeeder extends Seeder {
   run(em: EntityManager, context: DatabaseSeederContext): Promise<void> {
-    const seedUsers = users.map(() => {
-      // TODO: update Author to User once that is implemented
-      return new Author();
+    const seedUsers = users.map((it) => {
+      return new User(it);
     });
     context.users = seedUsers;
     return em.persistAndFlush(seedUsers);
