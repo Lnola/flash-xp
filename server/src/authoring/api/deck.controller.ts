12dcd8f32418e0771acd67999a88a1aa94da7fff
Lnola <nola.luka@gmail.com>
2025-07-13 19:15:12 +0200

    ✨ Create a fetchById endpoint on the deck controller

--- server/src/authoring/api/deck.controller.ts
+++ server/src/authoring/api/deck.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, NotFoundException, Param } from '@nestjs/common';
+import { Deck } from 'authoring/core/entities';
 import { DeckService } from 'authoring/core/services';
 
 @Controller('authoring/decks')
 export class DeckController {
   constructor(private readonly deckService: DeckService) {}
+
+  @Get(':id')
+  async fetchById(@Param('id') deckId: Deck['id']): Promise<Deck> {
+    const { error, data } = await this.deckService.fetchById(deckId);
+    if (error || !data) throw new NotFoundException(error);
+    return data;
+  }
 }
