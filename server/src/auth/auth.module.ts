f959b7ab7aa861f3a10dd4f2dfeb934a83175dc8
Lnola <nola.luka@gmail.com>
2025-07-13 17:31:22 +0200

    ✨ Create an auth module

---
+++ server/src/auth/auth.module.ts
@@ -0,0 +1,11 @@
+import { MikroOrmModule } from '@mikro-orm/nestjs';
+import { Module } from '@nestjs/common';
+import { User } from 'shared/entities';
+import { FirebaseAuthGuard, FirebaseAuthGuardProvider } from 'shared/guards';
+
+@Module({
+  imports: [MikroOrmModule.forFeature([User])],
+  providers: [FirebaseAuthGuard, FirebaseAuthGuardProvider],
+  exports: [FirebaseAuthGuard],
+})
+export class AuthModule {}
