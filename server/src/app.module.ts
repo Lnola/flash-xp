ad6a5cf4bcdded1ca89728b3d445244804624d0e
Lnola <nola.luka@gmail.com>
2025-07-13 18:12:27 +0200

    🚚 Move the auth module to the shared folder

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,6 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AuthModule } from 'auth/auth.module';
 import { CatalogModule } from 'catalog/catalog.module';
+import { AuthModule } from 'shared/auth/auth.module';
 import { DbConfigModule, OrmConfigModule } from 'shared/config';
 import { FirebaseProvider } from 'shared/providers';
 
