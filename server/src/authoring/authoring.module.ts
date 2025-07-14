4cb95e077cdbbd03af804e675427f96753ccb51e
Lnola <nola.luka@gmail.com>
2025-07-15 01:52:36 +0200

    ✨ Register the QuestionType repository used in the DeckService to the authoring module

--- server/src/authoring/authoring.module.ts
+++ server/src/authoring/authoring.module.ts
@@ -1,11 +1,11 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
 import { DeckController } from './api';
-import { Deck } from './core/entities';
+import { Deck, QuestionType } from './core/entities';
 import { DeckService } from './core/services';
 
 @Module({
-  imports: [MikroOrmModule.forFeature([Deck])],
+  imports: [MikroOrmModule.forFeature([Deck, QuestionType])],
   providers: [DeckService],
   controllers: [DeckController],
 })
