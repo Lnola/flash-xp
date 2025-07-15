2b503fb1e183c664b04aa8ce6f607e5c1d74e462
Lnola <nola.luka@gmail.com>
2025-07-15 02:47:30 +0200

    ✨ Register the QuestionTypeProvider to the AuthoringModule

--- server/src/authoring/authoring.module.ts
+++ server/src/authoring/authoring.module.ts
@@ -2,11 +2,12 @@ import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
 import { DeckController } from './api';
 import { Deck, QuestionType } from './core/entities';
+import { QuestionTypeProvider } from './core/providers';
 import { DeckService } from './core/services';
 
 @Module({
   imports: [MikroOrmModule.forFeature([Deck, QuestionType])],
-  providers: [DeckService],
+  providers: [QuestionTypeProvider, DeckService],
   controllers: [DeckController],
 })
 export class AuthoringModule {}
