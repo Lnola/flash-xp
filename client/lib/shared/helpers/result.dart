f7f1f8b3526d7c2e98f3dda784e61971d692a130
Lnola <nola.luka@gmail.com>
2025-07-19 01:08:44 +0200

    🧑‍💻 Create a Result helper class to encapsulate repository method results

---
+++ client/lib/shared/helpers/result.dart
@@ -0,0 +1,17 @@
+class Result<T> {
+  final bool success;
+  final T? data;
+  final String? error;
+
+  Result._({required this.success, this.data, this.error});
+
+  factory Result.success([T? data]) {
+    return Result._(success: true, data: data);
+  }
+
+  factory Result.failure(String error, [T? data]) {
+    return Result._(success: false, data: data, error: error);
+  }
+
+  void doSmn() {}
+}
