50693dbcb23cf2571b5860958e3b295a8f20ca10
Lnola <nola.luka@gmail.com>
2025-07-19 01:33:17 +0200

    ✨ Add the updateDeck method to the CreateApi

--- client/lib/features/create/data/create.api.dart
+++ client/lib/features/create/data/create.api.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:flashxp/features/create/data/dto/create_deck.dto.dart';
+import 'package:flashxp/features/create/data/dto/update_deck.dto.dart';
 import 'package:flashxp/shared/data/auth_http_client.dart';
 import 'package:http/http.dart' as http;
 
@@ -10,6 +11,13 @@ class CreateApi {
   Future<http.Response> createDeck(CreateDeckDto dto) {
     return client.post(
       client.buildUri('/authoring/decks'),
+      body: jsonEncode(dto.toJson()),
+    );
+  }
+
+  Future<http.Response> updateDeck(int deckId, UpdateDeckDto dto) {
+    return client.put(
+      client.buildUri('/authoring/decks/$deckId'),
       body: jsonEncode(dto.toJson()),
     );
   }
