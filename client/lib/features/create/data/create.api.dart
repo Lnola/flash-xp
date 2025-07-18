69448816458dde0b13020e69d837850978381ade
Lnola <nola.luka@gmail.com>
2025-07-19 00:52:43 +0200

    🧑‍💻 Create a CreateApi class to abstract the client server communication and implement createDeck

---
+++ client/lib/features/create/data/create.api.dart
@@ -0,0 +1,16 @@
+import 'dart:convert';
+
+import 'package:flashxp/features/create/data/dto/create_deck.dto.dart';
+import 'package:flashxp/shared/data/auth_http_client.dart';
+import 'package:http/http.dart' as http;
+
+class CreateApi {
+  final client = AuthHttpClient();
+
+  Future<http.Response> createDeck(CreateDeckDto deck) {
+    return client.post(
+      client.buildUri('/authoring/decks'),
+      body: jsonEncode(deck.toJson()),
+    );
+  }
+}
