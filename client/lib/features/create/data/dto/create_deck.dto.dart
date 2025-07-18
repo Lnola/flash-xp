7d74342b4edb63b30e283cada8c27518c03e4883
Lnola <nola.luka@gmail.com>
2025-07-19 01:32:50 +0200

    ♻️ Leverage the deck.dtos to extend create_deck.dtos

--- client/lib/features/create/data/dto/create_deck.dto.dart
+++ client/lib/features/create/data/dto/create_deck.dto.dart
@@ -1,53 +1,25 @@
-class CreateAnswerOptionDto {
-  final String text;
-  final bool isCorrect;
+import 'package:flashxp/features/create/data/dto/deck.dto.dart';
 
+class CreateAnswerOptionDto extends AnswerOptionDto {
   CreateAnswerOptionDto({
-    required this.text,
-    required this.isCorrect,
+    required super.text,
+    required super.isCorrect,
   });
-
-  Map<String, dynamic> toJson() => {
-        'text': text,
-        'isCorrect': isCorrect,
-      };
 }
 
-class CreateQuestionDto {
-  final String text;
-  final String? answer;
-  final String questionType;
-  final List<CreateAnswerOptionDto>? answerOptions;
-
+class CreateQuestionDto extends QuestionDto {
   CreateQuestionDto({
-    required this.text,
-    this.answer,
-    required this.questionType,
-    this.answerOptions,
+    required super.text,
+    super.answer,
+    required super.questionType,
+    super.answerOptions,
   });
-
-  Map<String, dynamic> toJson() => {
-        'text': text,
-        'answer': answer,
-        'questionType': questionType,
-        'answerOptions': answerOptions?.map((o) => o.toJson()).toList(),
-      };
 }
 
-class CreateDeckDto {
-  final String title;
-  final String description;
-  final List<CreateQuestionDto> questions;
-
+class CreateDeckDto extends DeckDto {
   CreateDeckDto({
-    required this.title,
-    required this.description,
-    required this.questions,
+    required super.title,
+    required super.description,
+    required super.questions,
   });
-
-  Map<String, dynamic> toJson() => {
-        'title': title,
-        'description': description,
-        'questions': questions.map((q) => q.toJson()).toList(),
-      };
 }
