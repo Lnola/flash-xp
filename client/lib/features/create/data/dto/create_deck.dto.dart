fb75868e8c915620384766591879270883ae188a
Lnola <nola.luka@gmail.com>
2025-07-19 01:39:01 +0200

    🏷️ Update dtos to be just typedefinitions instead of fully fledged implementations

--- client/lib/features/create/data/dto/create_deck.dto.dart
+++ client/lib/features/create/data/dto/create_deck.dto.dart
@@ -1,25 +1,5 @@
 import 'package:flashxp/features/create/data/dto/deck.dto.dart';
 
-class CreateAnswerOptionDto extends AnswerOptionDto {
-  CreateAnswerOptionDto({
-    required super.text,
-    required super.isCorrect,
-  });
-}
-
-class CreateQuestionDto extends QuestionDto {
-  CreateQuestionDto({
-    required super.text,
-    super.answer,
-    required super.questionType,
-    super.answerOptions,
-  });
-}
-
-class CreateDeckDto extends DeckDto {
-  CreateDeckDto({
-    required super.title,
-    required super.description,
-    required super.questions,
-  });
-}
+typedef CreateAnswerOptionDto = AnswerOptionDto;
+typedef CreateQuestionDto = QuestionDto;
+typedef CreateDeckDto = DeckDto;
--- client/lib/features/create/data/dto/update_deck.dto.dart
+++ client/lib/features/create/data/dto/update_deck.dto.dart
@@ -1,25 +1,5 @@
 import 'package:flashxp/features/create/data/dto/deck.dto.dart';
 
-class UpdateAnswerOptionDto extends AnswerOptionDto {
-  UpdateAnswerOptionDto({
-    required super.text,
-    required super.isCorrect,
-  });
-}
-
-class UpdateQuestionDto extends QuestionDto {
-  UpdateQuestionDto({
-    required super.text,
-    super.answer,
-    required super.questionType,
-    super.answerOptions,
-  });
-}
-
-class UpdateDeckDto extends DeckDto {
-  UpdateDeckDto({
-    required super.title,
-    required super.description,
-    required super.questions,
-  });
-}
+typedef UpdateAnswerOptionDto = AnswerOptionDto;
+typedef UpdateQuestionDto = QuestionDto;
+typedef UpdateDeckDto = DeckDto;
