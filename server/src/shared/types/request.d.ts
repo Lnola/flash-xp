f36a685970bd8aded6da132f12be09706ffee67e
Lnola <nola.luka@gmail.com>
2025-07-19 02:41:00 +0200

    🏷️ Add deck to the express request type

--- server/src/shared/types/request.d.ts
+++ server/src/shared/types/request.d.ts
@@ -1,9 +1,11 @@
 import { Request } from 'express';
+import { Deck } from 'authoring/core/entities';
 import { User } from 'shared/auth/entities';
 
 declare module 'express' {
   interface Request {
     ssoId: string;
     user: User;
+    deck?: Deck;
   }
 }
