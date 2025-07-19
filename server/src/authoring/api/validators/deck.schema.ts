f5a8c190fa1d81062db8c703cb3f92051adf1c6e
Lnola <nola.luka@gmail.com>
2025-07-19 18:21:10 +0200

    🦺 Add validation so that at least one answerOption must be correct

--- server/src/authoring/api/validators/deck.schema.ts
+++ server/src/authoring/api/validators/deck.schema.ts
@@ -6,11 +6,23 @@ const answerOptionSchema = z.object({
   isCorrect: z.boolean(),
 });
 
+const atLeastOneAnswerOptionCorrect = (
+  answerOptions: Array<{ text: string; isCorrect: boolean }> | null | undefined,
+) => {
+  if (!answerOptions) return true;
+  return answerOptions.some((option) => option.isCorrect);
+};
 const questionSchema = z.object({
   text: z.string(),
   answer: z.string().nullable().optional(),
   questionType: z.enum(QUESTION_TYPE_NAMES),
-  answerOptions: z.array(answerOptionSchema).nullable().optional(),
+  answerOptions: z
+    .array(answerOptionSchema)
+    .nullable()
+    .optional()
+    .refine(atLeastOneAnswerOptionCorrect, {
+      message: 'At least one answer option must be correct.',
+    }),
 });
 
 export const deckSchema = z.object({
