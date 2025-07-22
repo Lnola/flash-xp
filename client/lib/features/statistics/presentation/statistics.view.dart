577bc988367a82f89474f294c67a90f096a6d090
Lnola <nola.luka@gmail.com>
2025-07-22 22:38:51 +0200

    ♻️ Resolve the signOut todo

--- client/lib/features/statistics/presentation/statistics.view.dart
+++ client/lib/features/statistics/presentation/statistics.view.dart
@@ -7,16 +7,18 @@ import 'package:provider/provider.dart';
 class StatisticsView extends StatelessWidget {
   const StatisticsView({super.key});
 
+  void _signOut(BuildContext context) async {
+    final authService = context.read<AuthService>();
+    await authService.signOut();
+    if (!context.mounted) return;
+    context.go('/auth');
+  }
+
   @override
   Widget build(BuildContext context) {
-    final authService = context.read<AuthService>();
     return Center(
       child: FlashButton(
-        onPressed: () async {
-          await authService.signOut();
-          // TODO: Handle sign out properly
-          context.go('/auth');
-        },
+        onPressed: () => _signOut(context),
         label: 'Sign Out',
       ),
     );
