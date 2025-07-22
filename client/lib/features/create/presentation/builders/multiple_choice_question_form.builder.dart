639b2b852282087d4779634c07d94f7e2abe2960
Lnola <nola.luka@gmail.com>
2025-07-22 21:50:16 +0200

    🚚 Move the multiple choice question form to a standalone widget

--- client/lib/features/create/presentation/builders/multiple_choice_question_form.builder.dart
+++ client/lib/features/create/presentation/builders/multiple_choice_question_form.builder.dart
@@ -2,49 +2,18 @@ import 'package:flashxp/features/create/logic/controllers/multiple_choice.contro
 import 'package:flashxp/features/create/logic/create.controller.dart';
 import 'package:flashxp/features/create/presentation/builders/question_form.builder.dart';
 import 'package:flashxp/features/create/presentation/widgets/multiple_choice_legend.widget.dart';
-import 'package:flashxp/shared/presentation/widgets/input/flash_checkbox.dart';
-import 'package:flashxp/shared/presentation/widgets/input/flash_input_group.dart';
-import 'package:flashxp/shared/presentation/widgets/input/flash_text_input.dart';
+import 'package:flashxp/features/create/presentation/widgets/multiple_choice_question_form.widget.dart';
 import 'package:flutter/material.dart';
 
 class MultipleChoiceQuestionFormBuilder implements QuestionFormBuilder {
   @override
   Widget buildInputs(CreateController controller) {
-    return FlashInputGroup<MultipleChoiceController>(
-      inputControllers:
-          controller.questionsControllers as List<MultipleChoiceController>,
+    final questionsControllers =
+        controller.questionsControllers as List<MultipleChoiceController>;
+    return MultipleChoiceQuestionFormWidget(
+      questionsControllers: questionsControllers,
       onRemoveInputGroup: controller.removeQuestion,
-      isDirty: (input) =>
-          input.questionController.text.isNotEmpty ||
-          input.answerOptionsControllers.any(
-            (answerOptionController) =>
-                answerOptionController.text.text.isNotEmpty,
-          ),
-      buildInputs: (input) => [
-        FlashTextInput(
-          label: 'Question',
-          controller: input.questionController,
-        ),
-        for (var i = 0; i < 4; i++) ...[
-          Row(
-            children: [
-              Expanded(
-                child: FlashTextInput(
-                  label: 'Option ${String.fromCharCode(65 + i)}',
-                  controller: input.answerOptionsControllers[i].text,
-                ),
-              ),
-              Tooltip(
-                message: 'Is this answer correct?',
-                child: FlashCheckbox(
-                  value: input.answerOptionsControllers[i].isCorrect,
-                  onChanged: (_) => controller.toggleIsCorrect(input, i),
-                ),
-              ),
-            ],
-          ),
-        ],
-      ],
+      toggleIsAnswerOptionCorrect: controller.toggleIsCorrect,
     );
   }
 
---
+++ client/lib/features/create/presentation/widgets/multiple_choice_question_form.widget.dart
@@ -0,0 +1,61 @@
+import 'package:flashxp/features/create/logic/controllers/multiple_choice.controller.dart';
+import 'package:flashxp/shared/presentation/widgets/input/flash_checkbox.dart';
+import 'package:flashxp/shared/presentation/widgets/input/flash_input_group.dart';
+import 'package:flashxp/shared/presentation/widgets/input/flash_text_input.dart';
+import 'package:flutter/material.dart';
+
+class MultipleChoiceQuestionFormWidget extends StatelessWidget {
+  final List<MultipleChoiceController> questionsControllers;
+  final void Function(MultipleChoiceController) onRemoveInputGroup;
+  final void Function(MultipleChoiceController, int)
+      toggleIsAnswerOptionCorrect;
+
+  const MultipleChoiceQuestionFormWidget({
+    super.key,
+    required this.questionsControllers,
+    required this.onRemoveInputGroup,
+    required this.toggleIsAnswerOptionCorrect,
+  });
+
+  bool _isDirty(MultipleChoiceController input) {
+    return input.questionController.text.isNotEmpty ||
+        input.answerOptionsControllers.any(
+          (answerOptionController) =>
+              answerOptionController.text.text.isNotEmpty,
+        );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return FlashInputGroup<MultipleChoiceController>(
+      inputControllers: questionsControllers,
+      onRemoveInputGroup: onRemoveInputGroup,
+      isDirty: _isDirty,
+      buildInputs: (input) => [
+        FlashTextInput(
+          label: 'Question',
+          controller: input.questionController,
+        ),
+        for (var i = 0; i < 4; i++) ...[
+          Row(
+            children: [
+              Expanded(
+                child: FlashTextInput(
+                  label: 'Option ${String.fromCharCode(65 + i)}',
+                  controller: input.answerOptionsControllers[i].text,
+                ),
+              ),
+              Tooltip(
+                message: 'Is this answer correct?',
+                child: FlashCheckbox(
+                  value: input.answerOptionsControllers[i].isCorrect,
+                  onChanged: (_) => toggleIsAnswerOptionCorrect(input, i),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ],
+    );
+  }
+}
