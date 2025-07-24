440d37776409bf30fbdbfb7a0431fa6e68346309
Lnola <nola.luka@gmail.com>
2025-07-25 01:42:51 +0200

    🧑‍💻 Create fromDto methods to create controllers from dtos

--- client/lib/features/authoring/logic/controllers/multiple_choice.controller.dart
+++ client/lib/features/authoring/logic/controllers/multiple_choice.controller.dart
@@ -1,11 +1,20 @@
+import 'package:flashxp/features/authoring/data/dto/deck.dto.dart';
 import 'package:flutter/material.dart';
 
+// TODO: rename so its not plural
 class AnswerOptionControllers {
   final TextEditingController text = TextEditingController();
   bool isCorrect = false;
 
   void toggleIsCorrect() {
     isCorrect = !isCorrect;
+  }
+
+  static AnswerOptionControllers fromDto(AnswerOptionDto answerOptionDto) {
+    final controller = AnswerOptionControllers();
+    controller.text.text = answerOptionDto.text;
+    controller.isCorrect = answerOptionDto.isCorrect;
+    return controller;
   }
 }
 
@@ -19,5 +28,15 @@ class MultipleChoiceController {
     for (final answerOption in answerOptionsControllers) {
       answerOption.text.dispose();
     }
+  }
+
+  static MultipleChoiceController fromDto(QuestionDto questionDto) {
+    final controller = MultipleChoiceController();
+    controller.questionController.text = questionDto.text;
+    for (final answerOption in questionDto.answerOptions!) {
+      final optionController = AnswerOptionControllers.fromDto(answerOption);
+      controller.answerOptionsControllers.add(optionController);
+    }
+    return controller;
   }
 }
