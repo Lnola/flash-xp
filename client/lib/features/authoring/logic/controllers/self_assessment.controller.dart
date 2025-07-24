53eee757f7c0bed8fac1701ba63e9663f3b77a19
Lnola <nola.luka@gmail.com>
2025-07-25 01:47:23 +0200

    🧑‍💻 Create fromDto methods to create controllers from dtos

--- client/lib/features/authoring/logic/controllers/self_assessment.controller.dart
+++ client/lib/features/authoring/logic/controllers/self_assessment.controller.dart
@@ -1,3 +1,4 @@
+import 'package:flashxp/features/authoring/data/dto/deck.dto.dart';
 import 'package:flutter/material.dart';
 
 class SelfAssessmentController {
@@ -7,5 +8,12 @@ class SelfAssessmentController {
   void dispose() {
     questionController.dispose();
     answerController.dispose();
+  }
+
+  static SelfAssessmentController fromDto(QuestionDto dto) {
+    final controller = SelfAssessmentController();
+    controller.questionController.text = dto.text;
+    controller.answerController.text = dto.answer ?? '';
+    return controller;
   }
 }
