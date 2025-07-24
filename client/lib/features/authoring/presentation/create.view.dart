9636fae6a446394841343d71b801325a5677ad3e
Lnola <nola.luka@gmail.com>
2025-07-25 00:14:57 +0200

    ♻️ Extract the AuthoringFormWidget from the CreateView

--- client/lib/features/authoring/presentation/create.view.dart
+++ client/lib/features/authoring/presentation/create.view.dart
@@ -1,29 +1,8 @@
 import 'package:flashxp/features/authoring/data/authoring.repository.dart';
 import 'package:flashxp/features/authoring/logic/create.controller.dart';
-import 'package:flashxp/features/authoring/presentation/builders/multiple_choice_question_form.builder.dart';
-import 'package:flashxp/features/authoring/presentation/builders/question_form.builder.dart';
-import 'package:flashxp/features/authoring/presentation/builders/self_assessment_question_form.builder.dart';
-import 'package:flashxp/features/authoring/presentation/widgets/authoring_form_actions.widget.dart';
-import 'package:flashxp/features/authoring/presentation/widgets/practice_mode_select.widget.dart';
+import 'package:flashxp/features/authoring/presentation/widgets/authoring_form.widget.dart';
 import 'package:flashxp/shared/helpers/snackbar.dart';
-import 'package:flashxp/shared/logic/domain/practice_mode.enum.dart';
-import 'package:flashxp/shared/presentation/widgets/input/flash_text_input.dart';
 import 'package:flutter/material.dart';
-
-extension on PracticeMode {
-  QuestionFormBuilder get _builder => switch (this) {
-        PracticeMode.multipleChoice => MultipleChoiceQuestionFormBuilder(),
-        PracticeMode.selfAssessment => SelfAssessmentQuestionFormBuilder(),
-      };
-
-  Widget buildInputs(CreateController controller) => _builder.buildInputs(
-        controller.questionsControllers,
-        controller.removeQuestion,
-        controller.toggleIsCorrect,
-      );
-
-  Widget buildLegend() => _builder.buildLegend();
-}
 
 class CreateView extends StatefulWidget {
   const CreateView({super.key});
@@ -59,44 +38,9 @@ class CreateViewState extends State<CreateView> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        SingleChildScrollView(
-          clipBehavior: Clip.none,
-          padding: const EdgeInsets.only(top: 8, bottom: 120),
-          child: Column(
-            spacing: 8,
-            children: [
-              FlashTextInput(
-                label: 'Full title',
-                controller: controller.titleController,
-              ),
-              FlashTextInput(
-                label: 'Description',
-                controller: controller.descriptionController,
-              ),
-              const SizedBox(height: 8),
-              PracticeModeSelectWidget(
-                mode: controller.mode,
-                updateMode: controller.updateMode,
-              ),
-              const SizedBox(height: 8),
-              controller.mode.buildLegend(),
-              controller.mode.buildInputs(controller),
-              const SizedBox(height: 24),
-            ],
-          ),
-        ),
-        Positioned(
-          bottom: 0,
-          left: 0,
-          right: 0,
-          child: AuthoringFormActionsWidget(
-            addQuestion: controller.addQuestion,
-            submit: _submit,
-          ),
-        ),
-      ],
+    return AuthoringFormWidget(
+      controller: controller,
+      submit: _submit,
     );
   }
 }
