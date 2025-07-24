633582e5793170f30b1c060b156df8f2c760a93e
Lnola <nola.luka@gmail.com>
2025-07-24 23:29:00 +0200

    ♻️ Rename the base create route to authoring

--- client/lib/pages/authoring/create.routes.dart
+++ client/lib/pages/authoring/create.routes.dart
@@ -4,7 +4,7 @@ import 'package:flashxp/shared/presentation/layout/flash_layout.dart';
 import 'package:go_router/go_router.dart';
 
 final createRoutes = GoRoute(
-  path: '/create',
+  path: '/authoring',
   pageBuilder: (context, __) => RouteGuards.authGuard(
     context,
     child: const NoTransitionPage(
