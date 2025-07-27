e60f6fd504b28d43fb083d108d1a60cc42a9dd17
Lnola <nola.luka@gmail.com>
2025-07-27 16:57:20 +0200

    🚚 Move the PreviewApi to the shared/data/api folder

--- client/lib/features/preview/data/preview.repository.dart
+++ client/lib/features/preview/data/preview.repository.dart
@@ -1,15 +1,15 @@
 import 'dart:convert';
 
 import 'package:flashxp/features/preview/data/dto/deck.dto.dart';
-import 'package:flashxp/features/preview/data/preview.api.dart';
+import 'package:flashxp/shared/data/api/preview.api.dart';
 import 'package:flashxp/shared/helpers/result.dart';
 
 class PreviewRepository {
-  final api = PreviewApi();
+  final _previewApi = PreviewApi();
 
   Future<Result<DeckDto>> getDeck(int deckId) async {
     try {
-      final response = await api.getDeck(deckId);
+      final response = await _previewApi.getDeck(deckId);
       if (response.statusCode != 200) {
         final message = jsonDecode(response.body)['message'] ?? 'Unknown error';
         return Result.failure('Failed to fetch deck: $message');
