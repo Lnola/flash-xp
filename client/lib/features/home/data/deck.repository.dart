c234c411a45eff819a674c2fed78cce294ae3145
Lnola <nola.luka@gmail.com>
2025-08-01 22:31:19 +0200

    💡 Update client todos

--- client/lib/features/home/data/deck.repository.dart
+++ client/lib/features/home/data/deck.repository.dart
@@ -3,14 +3,12 @@ import 'dart:convert';
 import 'package:flashxp/shared/data/auth_http_client.dart';
 import 'package:flashxp/shared/data/dto/deck.dto.dart';
 
+// TODO: Implement this the way other repositories do it
 class DeckRepository {
   final client = AuthHttpClient();
 
   Future<List<DeckDto>> fetch() async {
-    // TODO: improve the fetch itself
     final response = await client.get(Uri.parse('http://localhost:3000/decks'));
-
-    // TODO: throw the correct error that comes from the server
     if (response.statusCode != 200) throw Exception('Failed to fetch decks.');
     return parseDecks(response.body);
   }
