b66eb027ebd9f17ffd90d08388557b58c720660f
Lnola <nola.luka@gmail.com>
2025-08-01 22:23:30 +0200

    🧑‍💻 Update to export all instead of manually declaring what is exported

--- server/src/catalog/core/entities/index.ts
+++ server/src/catalog/core/entities/index.ts
@@ -1,4 +1,4 @@
-export { CatalogDeck } from './catalog-deck.entity';
-export { CatalogQuestion } from './catalog-question.entity';
-export { Learner } from './learner.entity';
-export { Bookmark } from './bookmark.entity';
+export * from './catalog-deck.entity';
+export * from './catalog-question.entity';
+export * from './learner.entity';
+export * from './bookmark.entity';
