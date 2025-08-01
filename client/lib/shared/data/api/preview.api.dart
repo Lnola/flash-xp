465b10c42610b072d0ca620b0473ce3c8f5cc964
Lnola <nola.luka@gmail.com>
2025-08-01 21:13:07 +0200

    ✨ Add create and remove bookmark methods to the previewApi

--- client/lib/shared/data/api/preview.api.dart
+++ client/lib/shared/data/api/preview.api.dart
@@ -7,4 +7,12 @@ class PreviewApi {
   Future<http.Response> getDeck(int deckId) {
     return client.get(client.buildUri('/decks/$deckId'));
   }
+
+  Future<http.Response> createBookmark(int deckId) {
+    return client.post(client.buildUri('/decks/$deckId/bookmarks'));
+  }
+
+  Future<http.Response> removeBookmark(int deckId) {
+    return client.delete(client.buildUri('/decks/$deckId/bookmarks'));
+  }
 }
