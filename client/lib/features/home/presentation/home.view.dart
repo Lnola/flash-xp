ec241ce5fba827b5f54b5119d9cacce669a187a2
Lnola <nola.luka@gmail.com>
2025-08-01 21:33:34 +0200

    ⏪️ Revert the scroll implementation

--- client/lib/features/home/presentation/home.view.dart
+++ client/lib/features/home/presentation/home.view.dart
@@ -2,7 +2,6 @@ import 'package:flashxp/features/home/data/deck.repository.dart';
 import 'package:flashxp/features/home/logic/home.controller.dart';
 import 'package:flashxp/shared/presentation/widgets/flash_deck_card_grid.dart';
 import 'package:flutter/material.dart';
-import 'package:go_router/go_router.dart';
 
 class HomeView extends StatefulWidget {
   const HomeView({super.key});
@@ -13,13 +12,8 @@ class HomeView extends StatefulWidget {
 
 class _HomeViewState extends State<HomeView> {
   late final HomeController controller;
-  final scrollKey = GlobalKey();
-  bool _scrollPending = false;
 
-  void _onControllerUpdated() {
-    setState(() {});
-    _handleScroll();
-  }
+  void _onControllerUpdated() => setState(() {});
 
   @override
   void initState() {
@@ -29,34 +23,9 @@ class _HomeViewState extends State<HomeView> {
   }
 
   @override
-  void didChangeDependencies() {
-    super.didChangeDependencies();
-    final uri = GoRouterState.of(context).uri;
-    final shouldScrollToMyDecks = uri.queryParameters['scrollTo'] == 'myDecks';
-    if (shouldScrollToMyDecks) {
-      _scrollPending = true;
-    }
-  }
-
-  @override
   void dispose() {
     controller.removeListener(_onControllerUpdated);
     super.dispose();
-  }
-
-  void _handleScroll() {
-    if (_scrollPending && controller.myDecks.isNotEmpty) {
-      _scrollPending = false;
-      WidgetsBinding.instance.addPostFrameCallback((_) {
-        if (scrollKey.currentContext != null) {
-          Scrollable.ensureVisible(
-            scrollKey.currentContext!,
-            duration: const Duration(milliseconds: 300),
-            curve: Curves.easeInOut,
-          );
-        }
-      });
-    }
   }
 
   @override
@@ -73,7 +42,6 @@ class _HomeViewState extends State<HomeView> {
             isLoading: controller.isLoading,
           ),
           FlashDeckCardGrid(
-            key: scrollKey,
             title: 'My decks',
             decks: controller.myDecks,
             isLoading: controller.isLoading,
