4c984cfd98ce6bd564636691deb77ca731783387
Lnola <nola.luka@gmail.com>
2025-08-04 00:57:37 +0200

    ⚡️ Add questionCount to the CatalogDeck entity as a formula decorator

--- server/src/catalog/core/entities/catalog-deck.entity.ts
+++ server/src/catalog/core/entities/catalog-deck.entity.ts
@@ -1,4 +1,10 @@
-import { Collection, Entity, OneToMany, Property } from '@mikro-orm/core';
+import {
+  Collection,
+  Entity,
+  Formula,
+  OneToMany,
+  Property,
+} from '@mikro-orm/core';
 import BaseEntity from 'shared/database/base.entity';
 import { CatalogQuestion } from '.';
 
@@ -15,4 +21,15 @@ export class CatalogDeck extends BaseEntity {
 
   @OneToMany(() => CatalogQuestion, (question) => question.deck)
   questions? = new Collection<CatalogQuestion>(this);
+
+  // This value is calculated on every fetch. This behavior can be stopped
+  // by setting { lazy: true } here. However, that will require all uses to
+  // explicitly set { populate : ['questionCount'] }.
+  @Formula(
+    (alias) =>
+      `(SELECT CAST(COUNT(*) as INT) 
+        FROM question q 
+        WHERE q.deck_id = ${alias}.id)`,
+  )
+  questionCount!: number;
 }
