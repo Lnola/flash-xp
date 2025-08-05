c97b2843731b63526847390ee4ab90d367e4de42
Lnola <nola.luka@gmail.com>
2025-08-05 19:17:15 +0200

    🦺 Create the CatalogDeckQuery validation schema and type

---
+++ server/src/catalog/api/validators/catalog-deck-query.schema.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+import { QUESTION_TYPE_NAMES } from 'shared/constants';
+
+// TODO: think about naming and location of the CatalogDeckQuery type
+export const catalogDeckQuerySchema = z.object({
+  questionType: z.enum(QUESTION_TYPE_NAMES).optional(),
+  title: z.string().min(1).optional(),
+  popular: z.coerce.boolean().optional(),
+  bookmarked: z.coerce.boolean().optional(),
+  authored: z.coerce.boolean().optional(),
+  inProgress: z.coerce.boolean().optional(),
+  limit: z.coerce.number().int().min(1).optional().default(100),
+  offset: z.coerce.number().int().min(0).optional().default(0),
+});
+
+export type CatalogDeckQuery = z.infer<typeof catalogDeckQuerySchema>;
---
+++ server/src/catalog/api/validators/index.ts
@@ -0,0 +1,1 @@
+export * from './catalog-deck-query.schema';
