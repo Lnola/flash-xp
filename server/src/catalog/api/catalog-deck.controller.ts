31795cc0eb2d346e1f0594c0ba2adfa27d7998f1
Lnola <nola.luka@gmail.com>
2025-08-05 19:47:02 +0200

    ✨ Leverage the parseCatalogDeckQuery to fetch items instead of fetching all

--- server/src/catalog/api/catalog-deck.controller.ts
+++ server/src/catalog/api/catalog-deck.controller.ts
@@ -1,7 +1,13 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { CatalogDeck, Learner } from 'catalog/core/entities';
 import { CatalogDeckService } from 'catalog/core/services';
 import { User } from 'shared/decorators';
+import { ZodValidationPipe } from 'shared/pipes';
+import {
+  CatalogDeckQuery,
+  catalogDeckQuerySchema,
+  parseCatalogDeckQuery,
+} from './validators';
 
 // TODO: Think about this controller name, structure and contents
 @Controller('decks')
@@ -9,8 +15,13 @@ export class CatalogDeckController {
   constructor(private readonly catalogDeckService: CatalogDeckService) {}
 
   @Get()
-  fetchAll() {
-    return this.catalogDeckService.fetchAll();
+  fetch(
+    @Query(new ZodValidationPipe(catalogDeckQuerySchema))
+    query: CatalogDeckQuery,
+    @User('id') learnerId: Learner['id'],
+  ) {
+    const { where, pagination } = parseCatalogDeckQuery(query, learnerId);
+    return this.catalogDeckService.fetch(where, pagination);
   }
 
   @Get(':id')
