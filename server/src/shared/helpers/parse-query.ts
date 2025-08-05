d955273a6d67c0b84827138245d683d58f5dcff6
Lnola <nola.luka@gmail.com>
2025-08-05 19:15:10 +0200

    🏷️ Add types to parseQuery

--- server/src/shared/helpers/parse-query.ts
+++ server/src/shared/helpers/parse-query.ts
@@ -1,25 +1,24 @@
-type ParseQueryFilters = Record<string, unknown>;
 type ParseQueryPagination = {
   limit?: number;
   offset?: number;
 };
 type ParseQueryWhere = Record<string, unknown>;
 
-type ParseQueryParams = ParseQueryFilters & ParseQueryPagination;
-export type ParseQueryHandlers = Record<
+type ParseQueryParams<T> = T & ParseQueryPagination;
+export type ParseQueryHandlers<T> = Record<
   string,
-  (where: ParseQueryWhere, filters: ParseQueryFilters) => void
+  (where: ParseQueryWhere, filters: T) => void
 >;
 
-export const parseQuery = (
-  { limit, offset, ...filters }: ParseQueryParams,
-  handlers: ParseQueryHandlers,
-) => {
+export function parseQuery<T>(
+  { limit, offset, ...filters }: ParseQueryParams<T>,
+  handlers: ParseQueryHandlers<T>,
+) {
   const where: ParseQueryWhere = {};
   for (const key in filters) {
     if (handlers[key]) {
-      handlers[key](where, filters);
+      handlers[key](where, filters as T);
     }
   }
   return { where, pagination: { limit, offset } };
-};
+}
