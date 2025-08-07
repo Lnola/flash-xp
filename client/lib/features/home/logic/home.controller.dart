35d8da17637b7e30baf559a122c9be47e831fcc9
Lnola <nola.luka@gmail.com>
2025-08-08 00:22:06 +0200

    ♻️ Leverage and map the CatalogDeckModel in the home feature

--- client/lib/features/home/logic/home.controller.dart
+++ client/lib/features/home/logic/home.controller.dart
@@ -5,9 +5,9 @@ import 'package:flutter/material.dart';
 class HomeController extends ChangeNotifier {
   final DeckRepository _deckRepository;
 
-  List<DeckDto> inProgressDecks = [];
-  List<DeckDto> myDecks = [];
-  List<DeckDto> savedDecks = [];
+  List<CatalogDeckModel> inProgressDecks = [];
+  List<CatalogDeckModel> myDecks = [];
+  List<CatalogDeckModel> savedDecks = [];
   bool isLoading = true;
 
   HomeController(this._deckRepository) {
@@ -19,9 +19,9 @@ class HomeController extends ChangeNotifier {
     notifyListeners();
 
     try {
-      inProgressDecks = await _deckRepository.fetch();
-      myDecks = await _deckRepository.fetch(params: {'authored': 'true'});
-      savedDecks = await _deckRepository.fetch(params: {'bookmarked': 'true'});
+      inProgressDecks = await _getDecks();
+      myDecks = await _getDecks({'authored': 'true'});
+      savedDecks = await _getDecks({'bookmarked': 'true'});
     } catch (e) {
       // TODO: Handle error by showing a toast
       print(e);
@@ -29,5 +29,24 @@ class HomeController extends ChangeNotifier {
 
     isLoading = false;
     notifyListeners();
+  }
+
+  Future<List<CatalogDeckModel>> _getDecks([
+    Map<String, String>? queryParams,
+  ]) async {
+    final dtos = await _deckRepository.fetch(params: queryParams ?? {});
+    return _mapDtosToModels(dtos);
+  }
+
+  List<CatalogDeckModel> _mapDtosToModels(List<DeckDto> dtos) {
+    return dtos.map((dto) {
+      return CatalogDeckModel(
+        id: dto.id,
+        title: dto.title,
+        totalQuestions: dto.totalQuestions,
+        progress: dto.progress,
+        mode: dto.mode,
+      );
+    }).toList();
   }
 }
