7ef55687d3bb2af0a990e8433588fa16e49a6edc
Lnola <nola.luka@gmail.com>
2025-08-07 23:25:15 +0200

    ✨ Add getDecks to the DeckRepository

--- client/lib/features/explore/data/deck.repository.dart
+++ client/lib/features/explore/data/deck.repository.dart
@@ -1,10 +1,13 @@
 import 'dart:convert';
 
 import 'package:flashxp/features/explore/data/dto/deck.dto.dart';
+import 'package:flashxp/shared/data/api/catalog.api.dart';
 import 'package:flashxp/shared/data/auth_http_client.dart';
+import 'package:flashxp/shared/helpers/result.dart';
 
-// TODO: Implement this the way other repositories do it
 class DeckRepository {
+  final _catalogApi = CatalogApi();
+
   final client = AuthHttpClient();
 
   Future<List<DeckDto>> fetch({
@@ -23,5 +26,22 @@ class DeckRepository {
   List<DeckDto> parseDecks(String jsonString) {
     final List<dynamic> jsonList = json.decode(jsonString);
     return jsonList.map((json) => DeckDto.fromJson(json)).toList();
+  }
+
+  Future<Result<List<DeckDto>>> getDecks({
+    Map<String, String> queryParams = const {},
+  }) async {
+    try {
+      final response = await _catalogApi.getDecks(queryParams: queryParams);
+      if (response.statusCode != 200) {
+        final message = jsonDecode(response.body)['message'] ?? 'Unknown error';
+        return Result.failure('$message');
+      }
+      final List<dynamic> jsonList = jsonDecode(response.body);
+      final data = jsonList.map((it) => DeckDto.fromJson(it)).toList();
+      return Result.success(data);
+    } catch (error) {
+      return Result.failure(error.toString());
+    }
   }
 }
