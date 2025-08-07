4761c634af0cfa1ed994047535ba12ef4454856b
Lnola <nola.luka@gmail.com>
2025-08-08 00:27:24 +0200

    ♻️ Rewrite the DeckRepository as the HomeRepository following the example set in ExploreRepository

---
+++ client/lib/features/home/data/home.repository.dart
@@ -0,0 +1,26 @@
+import 'dart:convert';
+
+import 'package:flashxp/features/home/data/dto/deck.dto.dart';
+import 'package:flashxp/shared/data/api/catalog.api.dart';
+import 'package:flashxp/shared/helpers/result.dart';
+
+class HomeRepository {
+  final _catalogApi = CatalogApi();
+
+  Future<Result<List<DeckDto>>> getDecks({
+    Map<String, String> queryParams = const {},
+  }) async {
+    try {
+      final response = await _catalogApi.getDecks(queryParams: queryParams);
+      if (response.statusCode != 200) {
+        final message = jsonDecode(response.body)['message'] ?? 'Unknown error';
+        return Result.failure('$message');
+      }
+      final List<dynamic> jsonList = jsonDecode(response.body);
+      final data = jsonList.map((it) => DeckDto.fromJson(it)).toList();
+      return Result.success(data);
+    } catch (error) {
+      return Result.failure(error.toString());
+    }
+  }
+}
