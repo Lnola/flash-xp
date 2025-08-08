be332901a41aa9ec9e1d233a4a4b585552518f9f
Lnola <nola.luka@gmail.com>
2025-08-08 14:48:13 +0200

    ✨ Create the fetchQuestions enpoint on the QuickPracticeController

--- server/src/practice/api/quick-practice.controller.ts
+++ server/src/practice/api/quick-practice.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Param } from '@nestjs/common';
+import { PracticeQuestion } from 'practice/core/entities';
 import { QuickPracticeService } from 'practice/core/services';
 
 @Controller('practice/quick')
 export class QuickPracticeController {
   constructor(private readonly quickPracticeService: QuickPracticeService) {}
+
+  @Get('decks/:deckId/questions')
+  async fetchQuestions(
+    @Param('deckId') deckId: PracticeQuestion['deckId'],
+  ): Promise<PracticeQuestion[]> {
+    const { error, data } =
+      await this.quickPracticeService.fetchQuestions(deckId);
+    if (error || !data) throw new BadRequestException(error);
+    return data;
+  }
 }
