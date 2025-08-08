2402bcace3a9e6c369878487a5af0422c3cac5a1
Lnola <nola.luka@gmail.com>
2025-08-09 00:13:38 +0200

    ✨ Create an abstrace class PracticeTypeStrategy to facilitate different practice type implementations

---
+++ client/lib/features/practice/logic/domain/practice_type.strategy.dart
@@ -0,0 +1,6 @@
+import 'package:flashxp/features/practice/data/dto/question.dto.dart';
+import 'package:flashxp/shared/helpers/result.dart';
+
+abstract class PracticeTypeStrategy {
+  Future<Result<List<QuestionDto>>> getQuestions(int deckId);
+}
