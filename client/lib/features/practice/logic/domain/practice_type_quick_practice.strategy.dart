97a1c33a3f5b5123cc2cb957acd6a3450cfb5140
Lnola <nola.luka@gmail.com>
2025-08-09 00:13:55 +0200

    ✨ Implement the QuickPracticeStrategy

---
+++ client/lib/features/practice/logic/domain/practice_type_quick_practice.strategy.dart
@@ -0,0 +1,12 @@
+import 'package:flashxp/features/practice/data/dto/question.dto.dart';
+import 'package:flashxp/features/practice/data/quick_practice.repository.dart';
+import 'package:flashxp/features/practice/logic/domain/practice_type.strategy.dart';
+import 'package:flashxp/shared/helpers/result.dart';
+
+class QuickPracticeStrategy implements PracticeTypeStrategy {
+  final _quickPracticeRepository = QuickPracticeRepository();
+  @override
+  Future<Result<List<QuestionDto>>> getQuestions(int deckId) {
+    return _quickPracticeRepository.getQuestions(deckId);
+  }
+}
