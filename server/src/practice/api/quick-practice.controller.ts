57d7fbbd5cd9626e5831d80dd412efeeddb005d6
Lnola <nola.luka@gmail.com>
2025-08-08 14:47:34 +0200

    ✨ Inject the QuickPracticeService in the QuickPracticeController

--- server/src/practice/api/quick-practice.controller.ts
+++ server/src/practice/api/quick-practice.controller.ts
@@ -1,6 +1,7 @@
 import { Controller } from '@nestjs/common';
+import { QuickPracticeService } from 'practice/core/services';
 
 @Controller('practice/quick')
 export class QuickPracticeController {
-  constructor() {}
+  constructor(private readonly quickPracticeService: QuickPracticeService) {}
 }
