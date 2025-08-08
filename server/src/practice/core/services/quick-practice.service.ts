6f9193694952e8d1a28dce3354a0827837258e66
Lnola <nola.luka@gmail.com>
2025-08-08 14:46:38 +0200

    ✨ Add the fetchQuestions method to the QuickPracticeService

--- server/src/practice/core/services/quick-practice.service.ts
+++ server/src/practice/core/services/quick-practice.service.ts
@@ -2,6 +2,7 @@ import { InjectRepository } from '@mikro-orm/nestjs';
 import { Injectable } from '@nestjs/common';
 import { PracticeQuestion } from 'practice/core/entities';
 import { BaseEntityRepository } from 'shared/database/base.repository';
+import { Result } from 'shared/helpers/result';
 
 @Injectable()
 export class QuickPracticeService {
@@ -9,4 +10,18 @@ export class QuickPracticeService {
     @InjectRepository(PracticeQuestion)
     private readonly practiceQuestionRepository: BaseEntityRepository<PracticeQuestion>,
   ) {}
+
+  async fetchQuestions(
+    deckId: PracticeQuestion['deckId'],
+  ): Promise<Result<PracticeQuestion[]>> {
+    try {
+      const questions = await this.practiceQuestionRepository.find(
+        { deckId },
+        { populate: ['answerOptions'] },
+      );
+      return Result.success(questions);
+    } catch {
+      return Result.failure('Failed to fetch practice questions');
+    }
+  }
 }
