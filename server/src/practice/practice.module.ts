a7d160349496ea4ba413fee0e0f4736a57332176
Lnola <nola.luka@gmail.com>
2025-08-10 14:44:07 +0200

    ✨ Register the smart review controller and service to the practice module

--- server/src/practice/practice.module.ts
+++ server/src/practice/practice.module.ts
@@ -1,12 +1,12 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
-import { QuickPracticeController } from './api';
+import { QuickPracticeController, SmartReviewController } from './api';
 import { PracticeQuestion } from './core/entities';
-import { QuickPracticeService } from './core/services';
+import { QuickPracticeService, SmartReviewService } from './core/services';
 
 @Module({
   imports: [MikroOrmModule.forFeature([PracticeQuestion])],
-  providers: [QuickPracticeService],
-  controllers: [QuickPracticeController],
+  providers: [QuickPracticeService, SmartReviewService],
+  controllers: [QuickPracticeController, SmartReviewController],
 })
 export class PracticeModule {}
