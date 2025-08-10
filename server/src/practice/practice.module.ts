63c0b98701252639ab3b1a1faeaac1b23123f528
Lnola <nola.luka@gmail.com>
2025-08-10 22:04:03 +0200

    ✨ Add box to the practice module

--- server/src/practice/practice.module.ts
+++ server/src/practice/practice.module.ts
@@ -1,11 +1,11 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
 import { QuickPracticeController, SmartReviewController } from './api';
-import { PracticeQuestion } from './core/entities';
+import { Box, PracticeQuestion } from './core/entities';
 import { QuickPracticeService, SmartReviewService } from './core/services';
 
 @Module({
-  imports: [MikroOrmModule.forFeature([PracticeQuestion])],
+  imports: [MikroOrmModule.forFeature([Box, PracticeQuestion])],
   providers: [QuickPracticeService, SmartReviewService],
   controllers: [QuickPracticeController, SmartReviewController],
 })
