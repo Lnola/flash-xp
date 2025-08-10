a916e67cdd013accd5229f053a21f757a30f021a
Lnola <nola.luka@gmail.com>
2025-08-10 16:45:33 +0200

    ✨ Add learnerId to the fetchQuestions query

--- server/src/practice/api/smart-review.controller.ts
+++ server/src/practice/api/smart-review.controller.ts
@@ -1,6 +1,7 @@
 import { BadRequestException, Controller, Get, Param } from '@nestjs/common';
 import { PracticeQuestion } from 'practice/core/entities';
 import { SmartReviewService } from 'practice/core/services';
+import { User } from 'shared/decorators';
 
 @Controller('practice/smart')
 export class SmartReviewController {
@@ -9,9 +10,12 @@ export class SmartReviewController {
   @Get('decks/:deckId/questions')
   async fetchQuestions(
     @Param('deckId') deckId: PracticeQuestion['deckId'],
+    @User('id') learnerId: number,
   ): Promise<PracticeQuestion[]> {
-    const { error, data } =
-      await this.smartReviewService.fetchQuestions(deckId);
+    const { error, data } = await this.smartReviewService.fetchQuestions(
+      deckId,
+      learnerId,
+    );
     if (error || !data) throw new BadRequestException(error);
     return data;
   }
