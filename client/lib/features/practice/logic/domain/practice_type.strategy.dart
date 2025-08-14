92f478c5eb2614df63eb8a106177a890431e1ed0
Lnola <nola.luka@gmail.com>
2025-08-15 00:33:47 +0200

    ✨ Add handleCorrectAnswer to the PracticeTypeStrategy

--- client/lib/features/practice/logic/domain/practice_type.strategy.dart
+++ client/lib/features/practice/logic/domain/practice_type.strategy.dart
@@ -3,4 +3,5 @@ import 'package:flashxp/shared/helpers/result.dart';
 
 abstract class PracticeTypeStrategy {
   Future<Result<List<QuestionDto>>> getQuestions(int deckId);
+  Future<void> handleCorrectAnswer(int questionId);
 }
