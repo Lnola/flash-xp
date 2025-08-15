3ff0ccfc7125bb6f8f6fa55334ccb4f5032b897a
Lnola <nola.luka@gmail.com>
2025-08-15 02:28:39 +0200

    ✨ Add box index to the PreviewInfo widget

--- client/lib/features/preview/presentation/widgets/preview_info.widget.dart
+++ client/lib/features/preview/presentation/widgets/preview_info.widget.dart
@@ -1,41 +1,88 @@
+import 'package:flashxp/shared/presentation/widgets/utils/if.dart';
 import 'package:flutter/material.dart';
 
 class PreviewInfoWidget extends StatelessWidget {
   final String label;
   final String content;
+  final int? boxIndex;
 
   const PreviewInfoWidget({
     super.key,
     required this.label,
     required this.content,
+    this.boxIndex,
   });
 
   @override
   Widget build(BuildContext context) {
     final theme = Theme.of(context);
+    return IntrinsicHeight(
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          Expanded(
+            child: Container(
+              width: double.infinity,
+              decoration: BoxDecoration(
+                color: theme.colorScheme.surface,
+                borderRadius: BorderRadius.circular(28),
+              ),
+              padding: const EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    label,
+                    style: theme.textTheme.bodySmall?.copyWith(
+                      color: theme.colorScheme.onSurface.withAlpha(77),
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                  const SizedBox(height: 4),
+                  Text(
+                    content,
+                    style: theme.textTheme.bodyMedium?.copyWith(height: 1.2),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          If(
+            condition: boxIndex != null,
+            child: _BoxIndex(theme: theme, boxIndex: boxIndex),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _BoxIndex extends StatelessWidget {
+  const _BoxIndex({
+    required this.theme,
+    required this.boxIndex,
+  });
+
+  final ThemeData theme;
+  final int? boxIndex;
+
+  @override
+  Widget build(BuildContext context) {
     return Container(
-      width: double.infinity,
+      margin: const EdgeInsets.only(left: 8),
+      width: 44,
+      alignment: Alignment.center,
       decoration: BoxDecoration(
         color: theme.colorScheme.surface,
         borderRadius: BorderRadius.circular(28),
       ),
-      padding: const EdgeInsets.all(16),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            label,
-            style: theme.textTheme.bodySmall?.copyWith(
-              color: theme.colorScheme.onSurface.withAlpha(77),
-              fontWeight: FontWeight.w600,
-            ),
-          ),
-          const SizedBox(height: 4),
-          Text(
-            content,
-            style: theme.textTheme.bodyMedium?.copyWith(height: 1.2),
-          ),
-        ],
+      child: Text(
+        '$boxIndex',
+        style: theme.textTheme.bodySmall?.copyWith(
+          color: theme.colorScheme.onSurface.withAlpha(77),
+          fontWeight: FontWeight.w600,
+        ),
+        textAlign: TextAlign.center,
       ),
     );
   }
