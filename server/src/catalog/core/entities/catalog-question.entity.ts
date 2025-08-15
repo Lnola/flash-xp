bddcf24ca2688f8295c31360b49171ec560b39fc
Lnola <nola.luka@gmail.com>
2025-08-15 02:32:16 +0200

    🗃️ Add boxes to the CatalogQuestion entity as a OneToMany relation

--- server/src/catalog/core/entities/catalog-question.entity.ts
+++ server/src/catalog/core/entities/catalog-question.entity.ts
@@ -1,6 +1,12 @@
-import { Entity, ManyToOne, Property } from '@mikro-orm/core';
+import {
+  Collection,
+  Entity,
+  ManyToOne,
+  OneToMany,
+  Property,
+} from '@mikro-orm/core';
 import BaseEntity from 'shared/database/base.entity';
-import { CatalogDeck, CatalogQuestionType } from '.';
+import { CatalogBox, CatalogDeck, CatalogQuestionType } from '.';
 
 @Entity({ tableName: 'question' })
 export class CatalogQuestion extends BaseEntity {
@@ -15,4 +21,7 @@ export class CatalogQuestion extends BaseEntity {
 
   @ManyToOne(() => CatalogQuestionType, { eager: true })
   questionType!: CatalogQuestionType;
+
+  @OneToMany(() => CatalogBox, (box) => box.question)
+  boxes? = new Collection<CatalogBox>(this);
 }
