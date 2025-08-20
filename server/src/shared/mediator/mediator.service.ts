d6abc74db9fa6110b8f55001eaecb25371f421f7
Lnola <nola.luka@gmail.com>
2025-08-20 18:18:27 +0200

    🧑‍💻 Create a basic Mediator

---
+++ server/src/shared/mediator/mediator.service.ts
@@ -0,0 +1,29 @@
+import { EventEmitter } from 'events';
+import { Injectable, OnModuleDestroy } from '@nestjs/common';
+
+export interface IntegrationEvent<T = unknown> {
+  id: string;
+  type: string;
+  payload: T;
+  occurredAt: Date;
+}
+
+type EventHandler<T> = (event: IntegrationEvent<T>) => void;
+
+@Injectable()
+export class Mediator implements OnModuleDestroy {
+  private readonly bus = new EventEmitter();
+
+  publish<T>(event: IntegrationEvent<T>): void {
+    this.bus.emit(event.type, event);
+  }
+
+  subscribe<T>(type: string, handler: EventHandler<T>): () => void {
+    this.bus.on(type, handler);
+    return () => this.bus.off(type, handler);
+  }
+
+  onModuleDestroy() {
+    this.bus.removeAllListeners();
+  }
+}
