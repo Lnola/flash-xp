6c48777f824e64549a50294925ac7d8749ec87d6
Lnola <nola.luka@gmail.com>
2025-08-24 22:09:54 +0200

    ✨ Add the LearnerEvent entity to the module

--- server/src/learner-activity/learner-activity.module.ts
+++ server/src/learner-activity/learner-activity.module.ts
@@ -1,9 +1,10 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
+import { LearnerEvent } from './core/entities';
 import { AnswerSubmittedEventHandler } from './core/event-handlers';
 
 @Module({
-  imports: [MikroOrmModule.forFeature([])],
+  imports: [MikroOrmModule.forFeature([LearnerEvent])],
   providers: [AnswerSubmittedEventHandler],
   controllers: [],
 })
