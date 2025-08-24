4021a1c5bc2ed5c90f441e9b8732b89b1ef0911b
Lnola <nola.luka@gmail.com>
2025-08-25 00:43:47 +0200

    👔 Create the getDailyCorrectIncorrect method on the LearnerEventRepository

--- server/src/learner-activity/infrastructure/learner-event.repository.ts
+++ server/src/learner-activity/infrastructure/learner-event.repository.ts
@@ -3,9 +3,47 @@ import { Injectable } from '@nestjs/common';
 import { LearnerEvent } from 'learner-activity/core/entities';
 import { BaseEntityRepository } from 'shared/database/base.repository';
 
+// TODO: rename to statistics
 @Injectable()
 export class LearnerEventRepository extends BaseEntityRepository<LearnerEvent> {
   constructor(protected readonly em: EntityManager) {
     super(em, LearnerEvent);
   }
+
+  async getDailyCorrectIncorrect(
+    learnerId: number,
+    numberOfDays: number = 7,
+  ): Promise<DailyCorrectIncorrect[]> {
+    if (learnerId == null) throw new Error('learnerId required');
+
+    const knex = this.getKnex();
+    const rows = await knex<DailyCorrectIncorrect>('learner_event')
+      .select({
+        day: knex.raw(`TO_CHAR(created_at, 'Dy')`),
+        correct: knex.raw(
+          `COUNT(*) FILTER (WHERE (payload->>'isCorrect')::boolean = TRUE)`,
+        ),
+        incorrect: knex.raw(
+          `COUNT(*) FILTER (WHERE (payload->>'isCorrect')::boolean = FALSE)`,
+        ),
+      })
+      .where('learner_id', learnerId)
+      .andWhereRaw(`created_at >= NOW() - INTERVAL '${numberOfDays} days'`)
+      .groupBy('day')
+      .orderBy('day', 'asc');
+
+    const result = rows.map((row) => ({
+      day: String(row.day as string),
+      correct: Number(row.correct),
+      incorrect: Number(row.incorrect),
+    }));
+
+    return result;
+  }
 }
+
+export type DailyCorrectIncorrect = {
+  day: string;
+  correct: number;
+  incorrect: number;
+};
