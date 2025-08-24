5958de369350b9a7b860f4a7021c9a55127df98f
Lnola <nola.luka@gmail.com>
2025-08-24 22:10:49 +0200

    ✨ Register the LearnerEventService to the module

--- server/src/learner-activity/learner-activity.module.ts
+++ server/src/learner-activity/learner-activity.module.ts
@@ -2,10 +2,11 @@ import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
 import { LearnerEvent } from './core/entities';
 import { AnswerSubmittedEventHandler } from './core/event-handlers';
+import { LearnerEventService } from './core/services';
 
 @Module({
   imports: [MikroOrmModule.forFeature([LearnerEvent])],
-  providers: [AnswerSubmittedEventHandler],
+  providers: [AnswerSubmittedEventHandler, LearnerEventService],
   controllers: [],
 })
 export class LearnerActivityModule {}
