01259cde7ce4e6f659748adc42f2d7ce4fc788f8
Lnola <nola.luka@gmail.com>
2025-08-24 21:57:39 +0200

    ✨ Add the AnswerSubmittedEventHandler to the LearnerActivityModule providers

--- server/src/learner-activity/learner-activity.module.ts
+++ server/src/learner-activity/learner-activity.module.ts
@@ -1,9 +1,10 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
+import { AnswerSubmittedEventHandler } from './core/event-handlers';
 
 @Module({
   imports: [MikroOrmModule.forFeature([])],
-  providers: [],
+  providers: [AnswerSubmittedEventHandler],
   controllers: [],
 })
 export class LearnerActivityModule {}
