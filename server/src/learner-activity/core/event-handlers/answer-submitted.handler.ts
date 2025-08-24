b9c72df456598f073fb1f6099561217f4825d20d
Lnola <nola.luka@gmail.com>
2025-08-24 22:17:26 +0200

    ✨ Implement it so AnswerSubmittedEventHandler handle method calls the LearnerEventService create method

--- server/src/learner-activity/core/event-handlers/answer-submitted.handler.ts
+++ server/src/learner-activity/core/event-handlers/answer-submitted.handler.ts
@@ -1,14 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { LearnerEventService } from 'learner-activity/core/services';
 import { AnswerSubmittedEvent, BaseEventHandler } from 'shared/events';
 import { Mediator } from 'shared/mediator';
 
 @Injectable()
 export class AnswerSubmittedEventHandler extends BaseEventHandler<AnswerSubmittedEvent> {
-  constructor(mediator: Mediator) {
+  constructor(
+    private readonly learnerEventService: LearnerEventService,
+    mediator: Mediator,
+  ) {
     super(mediator, AnswerSubmittedEvent.name);
   }
 
-  handle(event: AnswerSubmittedEvent) {
-    console.log(event.payload);
+  // TODO: check if this needs to be async
+  handle({ payload }: AnswerSubmittedEvent) {
+    this.learnerEventService.create(payload);
   }
 }
