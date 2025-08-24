3eb3d5bfdbaa134356ef0f55ac6b8f8c2bda1e3f
Lnola <nola.luka@gmail.com>
2025-08-24 22:16:19 +0200

    ♻️ Remove the AnswerSubmittedEvent reference from the LearnerEventService

--- server/src/learner-activity/core/services/learner-event.service.ts
+++ server/src/learner-activity/core/services/learner-event.service.ts
@@ -2,7 +2,6 @@ import { InjectRepository } from '@mikro-orm/nestjs';
 import { Injectable } from '@nestjs/common';
 import { LearnerEvent } from 'learner-activity/core/entities';
 import { BaseEntityRepository } from 'shared/database/base.repository';
-import { AnswerSubmittedEvent } from 'shared/events';
 import { Result } from 'shared/helpers/result';
 
 @Injectable()
@@ -12,12 +11,23 @@ export class LearnerEventService {
     private readonly learnerEventRepository: BaseEntityRepository<LearnerEvent>,
   ) {}
 
-  create(event: AnswerSubmittedEvent): Result<void> {
+  create({
+    questionId,
+    learnerId,
+    isCorrect,
+  }: CreateLearnerEventPayload): Result<void> {
     try {
-      console.log(event);
+      console.log(questionId, learnerId, isCorrect);
       return Result.success();
     } catch {
       return Result.failure(`Failed to create learner event.`);
     }
   }
 }
+
+// TODO: update once LearnerEvent entity is updated
+type CreateLearnerEventPayload = {
+  questionId: number;
+  learnerId: number;
+  isCorrect: boolean;
+};
