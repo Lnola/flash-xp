12d17f9e4afe4a08681b5a9fc716f268bb4d88a4
Lnola <nola.luka@gmail.com>
2025-08-24 22:29:24 +0200

    ✨ Add props to the LearnerEvent entity

--- server/src/learner-activity/core/entities/learner-event.entity.ts
+++ server/src/learner-activity/core/entities/learner-event.entity.ts
@@ -1,5 +1,17 @@
-import { Entity } from '@mikro-orm/core';
+import { Entity, Property } from '@mikro-orm/core';
+import { LearnerEventType } from 'shared/constants';
 import BaseEntity from 'shared/database/base.entity';
 
 @Entity({ tableName: 'learner_event' })
-export class LearnerEvent extends BaseEntity {}
+export class LearnerEvent extends BaseEntity {
+  @Property()
+  learnerId!: number;
+
+  @Property()
+  type!: LearnerEventType;
+
+  @Property({ type: 'jsonb', nullable: true })
+  payload?: LearnerEventPayload;
+}
+
+type LearnerEventPayload = Record<string, boolean | number>;
