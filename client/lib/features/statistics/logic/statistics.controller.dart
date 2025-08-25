85efa035746df01c3f8a50607566eca3ef8dd88f
Lnola <nola.luka@gmail.com>
2025-08-25 22:13:24 +0200

    ✨ Create a StatController class

--- client/lib/features/statistics/logic/statistics.controller.dart
+++ client/lib/features/statistics/logic/statistics.controller.dart
@@ -1,6 +1,26 @@
 import 'package:flashxp/features/statistics/data/statistics.repository.dart';
 import 'package:flutter/material.dart';
 
+class StatController<T> {
+  bool isLoading = true;
+  String? error;
+  T? data;
+
+  StatController.loading()
+      : isLoading = true,
+        error = null,
+        data = null;
+  StatController.error(this.error)
+      : isLoading = false,
+        data = null;
+  StatController.data(this.data)
+      : isLoading = false,
+        error = null;
+}
+
+// To improve performance it would be great to break this
+// controller into multiple per widget controllers
+// each controlling their own loading, error and data
 class StatisticsController extends ChangeNotifier {
   final StatisticsRepository _statisticsRepository;
 
