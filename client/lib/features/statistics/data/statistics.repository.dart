774727a6aeaca3334db259204f8d7c8de1d9282f
Lnola <nola.luka@gmail.com>
2025-08-25 16:04:39 +0200

    ✨ Create the StatisticsRepository with the getDailyStreak method

---
+++ client/lib/features/statistics/data/statistics.repository.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+
+import 'package:flashxp/shared/data/api/statistics.api.dart';
+import 'package:flashxp/shared/helpers/result.dart';
+
+class StatisticsRepository {
+  final _statisticsApi = StatisticsApi();
+
+  Future<Result<int>> getDailyStreak() async {
+    try {
+      final response = await _statisticsApi.getDailyStreak();
+      if (response.statusCode != 200) {
+        final message = jsonDecode(response.body)['message'] ?? 'Unknown error';
+        return Result.failure(message);
+      }
+      final data = jsonDecode(response.body);
+      return Result.success(data);
+    } catch (error) {
+      return Result.failure(error.toString());
+    }
+  }
+}
