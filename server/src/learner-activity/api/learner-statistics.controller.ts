ac3b22dff859f5e3573121c98330ec4d5b3c47bf
Lnola <nola.luka@gmail.com>
2025-08-25 02:22:42 +0200

    ✨ Add the fetchAccuracyRate endpoint to the LearnerStatisticsController

--- server/src/learner-activity/api/learner-statistics.controller.ts
+++ server/src/learner-activity/api/learner-statistics.controller.ts
@@ -1,5 +1,9 @@
 import { Controller, Get, NotFoundException } from '@nestjs/common';
-import { DailyCorrectIncorrect } from 'learner-activity/core/models';
+import { LearnerEvent } from 'learner-activity/core/entities';
+import {
+  AccuracyRate,
+  DailyCorrectIncorrect,
+} from 'learner-activity/core/models';
 import { LearnerStatisticsService } from 'learner-activity/core/services';
 import { User } from 'shared/decorators';
 
@@ -11,10 +15,20 @@ export class LearnerStatisticsController {
 
   @Get('daily-correct-incorrect')
   async fetchDailyCorrectIncorrect(
-    @User('id') learnerId: number,
+    @User('id') learnerId: LearnerEvent['learnerId'],
   ): Promise<DailyCorrectIncorrect[]> {
     const { error, data } =
       await this.learnerStatisticsService.fetchDailyCorrectIncorrect(learnerId);
+    if (error || !data) throw new NotFoundException(error);
+    return data;
+  }
+
+  @Get('accuracy-rate')
+  async fetchAccuracyRate(
+    @User('id') learnerId: LearnerEvent['learnerId'],
+  ): Promise<AccuracyRate> {
+    const { error, data } =
+      await this.learnerStatisticsService.fetchAccuracyRate(learnerId);
     if (error || !data) throw new NotFoundException(error);
     return data;
   }
