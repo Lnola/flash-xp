7d78eb8ff1870c1eb65c1d1b8c6076a344e9ce2b
Lnola <nola.luka@gmail.com>
2025-08-25 02:59:24 +0200

    ✨ Add the fetchAnswersCount method to the LearnerStatisticsService

--- server/src/learner-activity/core/services/learner-statistics.service.ts
+++ server/src/learner-activity/core/services/learner-statistics.service.ts
@@ -1,3 +1,4 @@
+import { InjectRepository } from '@mikro-orm/nestjs';
 import { Injectable } from '@nestjs/common';
 import { LearnerEvent } from 'learner-activity/core/entities';
 import {
@@ -5,13 +6,32 @@ import {
   DailyCorrectIncorrect,
 } from 'learner-activity/core/models';
 import { LearnerStatisticsRepository } from 'learner-activity/infrastructure';
+import { BaseEntityRepository } from 'shared/database/base.repository';
 import { Result } from 'shared/helpers/result';
 
 @Injectable()
 export class LearnerStatisticsService {
   constructor(
+    @InjectRepository(LearnerEvent)
+    private readonly learnerEventRepository: BaseEntityRepository<LearnerEvent>,
     private readonly learnerStatisticsRepository: LearnerStatisticsRepository,
   ) {}
+
+  async fetchAnswersCount(
+    learnerId: LearnerEvent['learnerId'],
+    interval?: number,
+  ): Promise<Result<number>> {
+    try {
+      const count = await this.learnerStatisticsRepository.getAnswersCount(
+        learnerId,
+        interval,
+      );
+      return Result.success(count);
+    } catch (error) {
+      console.log(error);
+      return Result.failure(`Failed to fetch answers count.`);
+    }
+  }
 
   async fetchDailyCorrectIncorrect(
     learnerId: LearnerEvent['learnerId'],
