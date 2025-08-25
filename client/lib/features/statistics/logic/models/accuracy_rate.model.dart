f2a0e05bf648e5ab55820665a4c932def8b7c465
Lnola <nola.luka@gmail.com>
2025-08-26 00:51:58 +0200

    ✨ Create the AccuracyRate model

---
+++ client/lib/features/statistics/logic/models/accuracy_rate.model.dart
@@ -0,0 +1,19 @@
+class AccuracyRate {
+  final int correct;
+  final int total;
+  final double value;
+
+  AccuracyRate({
+    required this.correct,
+    required this.total,
+    required this.value,
+  });
+
+  factory AccuracyRate.fromJson(Map<String, dynamic> json) {
+    return AccuracyRate(
+      correct: json['correct'],
+      total: json['total'],
+      value: json['value']?.toDouble(),
+    );
+  }
+}
