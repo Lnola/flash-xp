673e39e20266bb97af568c6b4f03aa9c0f6a0ca6
Lnola <nola.luka@gmail.com>
2025-08-25 23:27:31 +0200

    ✨ Add the getAnswerCount method to the StatisticsRepository

--- client/lib/features/statistics/data/statistics.repository.dart
+++ client/lib/features/statistics/data/statistics.repository.dart
@@ -19,4 +19,22 @@ class StatisticsRepository {
       return Result.failure(error.toString());
     }
   }
+
+  Future<Result<int>> getAnswerCount({
+    Map<String, String> queryParams = const {},
+  }) async {
+    try {
+      final response = await _statisticsApi.getAnswerCount(
+        queryParams: queryParams,
+      );
+      if (response.statusCode != 200) {
+        final message = jsonDecode(response.body)['message'] ?? 'Unknown error';
+        return Result.failure(message);
+      }
+      final data = jsonDecode(response.body);
+      return Result.success(data);
+    } catch (error) {
+      return Result.failure(error.toString());
+    }
+  }
 }
