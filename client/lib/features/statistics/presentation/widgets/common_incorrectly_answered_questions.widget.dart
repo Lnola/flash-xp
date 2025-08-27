2b78a39478e2307d58027fc2781afc2c8619e6f3
Lnola <nola.luka@gmail.com>
2025-08-27 05:46:45 +0200

    🚸 Add a legend to the common incorrect widget

--- client/lib/features/statistics/presentation/widgets/common_incorrectly_answered_questions.widget.dart
+++ client/lib/features/statistics/presentation/widgets/common_incorrectly_answered_questions.widget.dart
@@ -24,10 +24,41 @@ class CommonIncorrectlyAnsweredQuestionsWidget extends StatelessWidget {
           ),
         )
         .toList();
-    return QuestionSummaryListWidget(
-      items: items,
-      isLoading: incorrectlyAnsweredQuestions.isLoading,
-      error: incorrectlyAnsweredQuestions.error,
+    return Column(
+      children: [
+        const _Legend(),
+        const SizedBox(height: 12),
+        QuestionSummaryListWidget(
+          items: items,
+          isLoading: incorrectlyAnsweredQuestions.isLoading,
+          error: incorrectlyAnsweredQuestions.error,
+        ),
+      ],
+    );
+  }
+}
+
+class _Legend extends StatelessWidget {
+  const _Legend();
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      spacing: 8,
+      children: [
+        Text(
+          'Questions you frequently get wrong.',
+          style: Theme.of(context).textTheme.titleSmall,
+        ),
+        Text(
+          'The number next to the deck title and question text is the total number of mistakes you made on that specific question.\nClick on the card below for an overview of the deck and practice.',
+          style: Theme.of(context).textTheme.bodySmall?.copyWith(
+                color: Theme.of(context).colorScheme.onSurface.withAlpha(77),
+                fontWeight: FontWeight.w600,
+              ),
+        ),
+      ],
     );
   }
 }
