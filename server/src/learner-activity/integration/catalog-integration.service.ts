574db1cad8be51eb594bda530cadbc6e437eaf14
Lnola <nola.luka@gmail.com>
2025-08-27 03:38:35 +0200

    ♻️ Update getQuestionSummaries integration to use the fetch method instead of the removed fetchSummaries

--- server/src/learner-activity/integration/catalog-integration.service.ts
+++ server/src/learner-activity/integration/catalog-integration.service.ts
@@ -11,8 +11,8 @@ export class CatalogIntegrationService {
 
   async getQuestionSummaries(ids: number[]): Promise<QuestionSummary[]> {
     if (!ids.length) throw new Error('No IDs provided');
-    const { error, data } =
-      await this.catalogExternalService.fetchSummaries(ids);
+    const where = { id: { $in: ids } };
+    const { error, data } = await this.catalogExternalService.fetch(where);
     if (error) throw new Error(error);
     return data!;
   }
