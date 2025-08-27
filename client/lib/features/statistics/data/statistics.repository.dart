cdeb14ea7a9d951c8906f36145a046f04d01f619
Lnola <nola.luka@gmail.com>
2025-08-27 07:54:37 +0200

    ✨ Create the getQuestionTypeOccurrenceCount method on the StatisticsRepository

--- client/lib/features/statistics/data/statistics.repository.dart
+++ client/lib/features/statistics/data/statistics.repository.dart
@@ -3,6 +3,7 @@ import 'dart:convert';
 import 'package:flashxp/features/statistics/logic/models/accuracy_rate.model.dart';
 import 'package:flashxp/features/statistics/logic/models/daily_correct_incorrect.model.dart';
 import 'package:flashxp/features/statistics/logic/models/incorrectly_answered_questions.model.dart';
+import 'package:flashxp/features/statistics/logic/models/question_type_occurrence_count.model.dart';
 import 'package:flashxp/shared/data/api/statistics.api.dart';
 import 'package:flashxp/shared/helpers/result.dart';
 
@@ -101,6 +102,22 @@ class StatisticsRepository {
       final data = (jsonDecode(response.body) as List)
           .map((item) => IncorrectlyAnsweredQuestions.fromJson(item))
           .toList();
+      return Result.success(data);
+    } catch (error) {
+      return Result.failure(error.toString());
+    }
+  }
+
+  Future<Result<QuestionTypeOccurrenceCount>>
+      getQuestionTypeOccurrenceCount() async {
+    try {
+      final response = await _statisticsApi.getQuestionTypeOccurrenceCount();
+      if (response.statusCode != 200) {
+        final message = jsonDecode(response.body)['message'] ?? 'Unknown error';
+        return Result.failure(message);
+      }
+      final data =
+          QuestionTypeOccurrenceCount.fromJson(jsonDecode(response.body));
       return Result.success(data);
     } catch (error) {
       return Result.failure(error.toString());
