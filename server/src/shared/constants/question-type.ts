349d3b77348a5c538316d562b16b71176ca54da3
Lnola <nola.luka@gmail.com>
2025-08-27 07:29:46 +0200

    🧑‍💻 Add a QUESTION_TYPE_MAP const for ease of use and to avoid repetition

--- server/src/shared/constants/question-type.ts
+++ server/src/shared/constants/question-type.ts
@@ -2,3 +2,8 @@ export const QUESTION_TYPE_NAMES = [
   'Multiple Choice',
   'Self Assessment',
 ] as const;
+
+export const QUESTION_TYPE_MAP = {
+  multipleChoice: QUESTION_TYPE_NAMES[0],
+  selfAssessment: QUESTION_TYPE_NAMES[1],
+} as const;
