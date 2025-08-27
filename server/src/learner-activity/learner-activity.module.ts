f036629e6721350c7c720e4778aaad1634a9a450
Lnola <nola.luka@gmail.com>
2025-08-27 03:26:11 +0200

    🏗️ Import the CatalogIntegrationModule to the LearnerActivityModule

--- server/src/learner-activity/learner-activity.module.ts
+++ server/src/learner-activity/learner-activity.module.ts
@@ -1,14 +1,17 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
-import { CatalogExternalModule } from 'catalog/external';
 import { LearnerStatisticsController } from './api';
 import { LearnerEvent } from './core/entities';
 import { AnswerSubmittedEventHandler } from './core/event-handlers';
 import { LearnerEventService, LearnerStatisticsService } from './core/services';
 import { LearnerStatisticsRepository } from './infrastructure';
+import { CatalogIntegrationModule } from './integration';
 
 @Module({
-  imports: [MikroOrmModule.forFeature([LearnerEvent]), CatalogExternalModule],
+  imports: [
+    MikroOrmModule.forFeature([LearnerEvent]),
+    CatalogIntegrationModule,
+  ],
   providers: [
     AnswerSubmittedEventHandler,
     LearnerEventService,
