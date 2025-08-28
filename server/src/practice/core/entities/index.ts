af3225304b24624b040ddecc9353b08ff297817e
Lnola <nola.luka@gmail.com>
2025-08-28 18:03:39 +0200

    🗃️ Create the LearnerDeckProgress entity

--- server/src/practice/core/entities/index.ts
+++ server/src/practice/core/entities/index.ts
@@ -1,3 +1,4 @@
 export * from './box.entity';
+export * from './learner-deck-progress.entity';
 export * from './practice-answer-option.entity';
 export * from './practice-question.entity';
---
+++ server/src/practice/core/entities/learner-deck-progress.entity.ts
@@ -0,0 +1,13 @@
+import { Entity, Property } from '@mikro-orm/core';
+
+@Entity({ expression: 'select * from learner_deck_progress_view' })
+export class LearnerDeckProgress {
+  @Property()
+  learnerId!: number;
+
+  @Property()
+  deckId!: number;
+
+  @Property({ type: 'decimal' })
+  progress!: number;
+}
