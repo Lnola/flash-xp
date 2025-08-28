03c5c18fb0a6b82522df40a7f63e363d5acb6299
Lnola <nola.luka@gmail.com>
2025-08-29 00:38:54 +0200

    💡 Remove the todo

--- server/src/catalog/api/validators/catalog-deck-query.parser.ts
+++ server/src/catalog/api/validators/catalog-deck-query.parser.ts
@@ -7,7 +7,6 @@ export const parseCatalogDeckQuery = (
   learnerId?: Learner['id'],
 ) => {
   const handlers: ParseQueryHandlers<CatalogDeckQuery, CatalogDeck> = {
-    // TODO: implement the popular handler
     questionType: (where, filters) => {
       where.questions = { questionType: { name: filters.questionType } };
     },
