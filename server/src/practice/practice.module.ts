aa6137fc84df533ae3e4e999927a84b44a7cce64
Lnola <nola.luka@gmail.com>
2025-08-28 18:03:57 +0200

    ✨ Add LearnerDeckProgress to the practice module

--- server/src/practice/practice.module.ts
+++ server/src/practice/practice.module.ts
@@ -1,12 +1,14 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
 import { QuickPracticeController, SmartReviewController } from './api';
-import { Box, PracticeQuestion } from './core/entities';
+import { Box, LearnerDeckProgress, PracticeQuestion } from './core/entities';
 import { QuickPracticeService, SmartReviewService } from './core/services';
 import { PracticeQuestionRepository } from './infrastructure';
 
 @Module({
-  imports: [MikroOrmModule.forFeature([Box, PracticeQuestion])],
+  imports: [
+    MikroOrmModule.forFeature([Box, PracticeQuestion, LearnerDeckProgress]),
+  ],
   providers: [
     QuickPracticeService,
     SmartReviewService,
