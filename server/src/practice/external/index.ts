808382582730fe3b9fcf22f1ec640d137da35f73
Lnola <nola.luka@gmail.com>
2025-08-28 20:06:28 +0200

    ✨ Create the PracticeExternalService that exposes the practice progress service fetchInProgressDecks method

---
+++ server/src/practice/external/index.ts
@@ -0,0 +1,1 @@
+export * from './practice-external.service';
---
+++ server/src/practice/external/practice-external.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@nestjs/common';
+import { PracticeProgress } from 'practice/core/entities';
+import { PracticeProgressService } from 'practice/core/services';
+import { Result } from 'shared/helpers/result';
+
+@Injectable()
+export class PracticeExternalService {
+  constructor(
+    private readonly practiceProgressService: PracticeProgressService,
+  ) {}
+
+  async fetchInProgressDecks(
+    learnerId: PracticeProgress['learnerId'],
+  ): Promise<Result<PracticeProgress[]>> {
+    return this.practiceProgressService.fetchInProgressDecks(learnerId);
+  }
+}
