e80a1df2884ddecdc30e9c557900d26c67cdc68f
Lnola <nola.luka@gmail.com>
2025-08-28 20:15:38 +0200

    🐛 Fix an old reference to PracticeProgress

--- server/src/practice/practice.module.ts
+++ server/src/practice/practice.module.ts
@@ -1,13 +1,13 @@
 import { MikroOrmModule } from '@mikro-orm/nestjs';
 import { Module } from '@nestjs/common';
 import { QuickPracticeController, SmartReviewController } from './api';
-import { Box, LearnerDeckProgress, PracticeQuestion } from './core/entities';
+import { Box, PracticeProgress, PracticeQuestion } from './core/entities';
 import { QuickPracticeService, SmartReviewService } from './core/services';
 import { PracticeQuestionRepository } from './infrastructure';
 
 @Module({
   imports: [
-    MikroOrmModule.forFeature([Box, PracticeQuestion, LearnerDeckProgress]),
+    MikroOrmModule.forFeature([Box, PracticeQuestion, PracticeProgress]),
   ],
   providers: [
     QuickPracticeService,
