949dc2e41f16ff944440f604e1a2d731de21d2b4
Lnola <nola.luka@gmail.com>
2025-08-28 18:01:05 +0200

    🗃️ Add a migration to create the learner_deck_progress_view

---
+++ server/src/shared/database/migrations/20250828155333-create-learner-deck-progress-view.ts
@@ -0,0 +1,54 @@
+import { Migration } from '@mikro-orm/migrations';
+
+const LEARNER_DECK_PROGRESS_VIEW = 'learner_deck_progress_view';
+const WEIGHTS = [0, 0.25, 0.5, 0.75, 1];
+const ROUNDING_PRECISION = 2;
+
+export class CreateLearnerDeckProgressView extends Migration {
+  override up(): void {
+    const weights = WEIGHTS.map(
+      (value, index) => `(${index + 1}, ${value})`,
+    ).join(', ');
+    const createLearnerDeckProgressView = `
+      CREATE OR REPLACE VIEW learner_deck_progress_view AS
+      WITH weights AS (
+        SELECT * 
+        FROM (VALUES ${weights}) AS t (index, value)
+      ),
+      question_count_per_box AS (
+        SELECT
+          box.learner_id,
+          box.deck_id,
+          box.index,
+          COUNT(*) AS count
+        FROM box
+        GROUP BY box.learner_id, box.deck_id, box.index
+      ),
+      question_progress_per_box AS (
+        SELECT
+          qc.learner_id,
+          qc.deck_id,
+          qc.index,
+          qc.count, 
+          (w.value * qc.count) AS progress
+        FROM question_count_per_box AS qc
+        LEFT JOIN weights AS w
+        ON w.index = qc.index
+      )
+      SELECT 
+        qp.learner_id,
+        qp.deck_id,
+        ROUND(SUM(qp.progress) / SUM(qp.count) * 100, ${ROUNDING_PRECISION}) AS progress
+      FROM question_progress_per_box AS qp
+      GROUP BY qp.learner_id, qp.deck_id;`;
+    this.addSql(createLearnerDeckProgressView);
+  }
+
+  override down(): void {
+    const knex = this.getKnex();
+    const dropLearnerDeckProgressView = knex.schema.dropView(
+      LEARNER_DECK_PROGRESS_VIEW,
+    );
+    this.addSql(dropLearnerDeckProgressView.toQuery());
+  }
+}
